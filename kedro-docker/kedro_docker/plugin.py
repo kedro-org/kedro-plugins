2a53167a58298fd2f65e657a7db17f60cc664608
Merel Theisen <merel.theisen@quantumblack.com>
2022-01-19 11:00:46 +0000

    Fix bandit scan
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- kedro-docker/kedro_docker/plugin.py
+++ kedro-docker/kedro_docker/plugin.py
@@ -127,7 +127,9 @@ def docker_init(spark):
     if KEDRO_VERSION.match(">=0.17.0"):
         verbose = KedroCliError.VERBOSE_ERROR
     else:
-        from kedro.framework.cli.cli import _VERBOSE as verbose  # pylint:disable=import-outside-toplevel, no-name-in-module
+        from kedro.framework.cli.cli import (  # noqa # pylint:disable=import-outside-toplevel, no-name-in-module
+            _VERBOSE as verbose,
+        )
 
     docker_file_version = "spark" if spark else "simple"
     docker_file = f"Dockerfile.{docker_file_version}"

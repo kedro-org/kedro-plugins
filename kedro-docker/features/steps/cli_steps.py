c211fa3163d8794ef7dc9e418d88e78a9883ae55
Merel Theisen <merel.theisen@quantumblack.com>
2022-04-01 11:51:49 +0100

    Try using path again in e2e test for windows compatibility
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- kedro-docker/features/steps/cli_steps.py
+++ kedro-docker/features/steps/cli_steps.py
@@ -178,9 +178,9 @@ def exec_kedro_command(context, command):
 @given("I have installed the project dependencies")
 def pip_install_dependencies(context):
     """Install project dependencies using pip."""
-    reqs_path = "src/requirements.txt"
+    reqs_path = Path("src", "requirements.txt")
     res = run(
-        [context.pip, "install", "-r", reqs_path],
+        [context.pip, "install", "-r", str(reqs_path)],
         env=context.env,
         cwd=str(context.root_project_dir),
     )

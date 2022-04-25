42b6ff3ce02f42ad51b9fa6234f410d12f1c208f
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2022-04-25 15:03:35 +0100

    lint

--- kedro-telemetry/kedro_telemetry/plugin.py
+++ kedro-telemetry/kedro_telemetry/plugin.py
@@ -17,6 +17,7 @@ import yaml
 from kedro.framework.cli.cli import KedroCLI
 from kedro.framework.cli.hooks import cli_hook_impl
 from kedro.framework.startup import ProjectMetadata
+
 from kedro_telemetry import __version__ as telemetry_version
 from kedro_telemetry.masking import _get_cli_structure, _mask_kedro_cli
 
@@ -204,8 +205,7 @@ def _confirm_consent(telemetry_file_path: Path) -> bool:
             return False
     except Exception as exc:  # pylint: disable=broad-except
         logger.warning(
-            "Failed to confirm consent. Exception: %s",
-            exc,
+            "Failed to confirm consent. Exception: %s", exc,
         )
         return False
 

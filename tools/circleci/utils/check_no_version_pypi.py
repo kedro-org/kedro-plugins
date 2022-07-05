52c69d7ca79bf41660da6d7028e6e47bb5d05816
Nok Chan <nok.lam.chan@quantumblack.com>
2022-07-05 14:07:11 +0100

    Make the logging niceer
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- tools/circleci/utils/check_no_version_pypi.py
+++ tools/circleci/utils/check_no_version_pypi.py
@@ -1,17 +1,14 @@
 import requests
-import sys
+
 
 
 def check_no_version_pypi(pypi_endpoint, package_name, package_version):
-    print("Check if package is on pypi")
+    print("Check if {package_name} {package_version} is on pypi")
     response = requests.get(pypi_endpoint)
     if response.status_code == 404:
         # Not exist on Pypi - do release
         print(f"Starting the release of {package_name} {package_version}")
         return True
-
     else:
         print(f"Skipped: {package_name} {package_version} already exists on PyPI")
-        # sys.exit(0)  # This will terminate the CI job
         return False
-

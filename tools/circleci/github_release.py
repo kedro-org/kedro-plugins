efedb0e81c983d355476e52cd76db9c7c22af679
Nok Chan <nok.lam.chan@quantumblack.com>
2022-07-05 15:44:21 +0100

    Try trigger release
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- tools/circleci/github_release.py
+++ tools/circleci/github_release.py
@@ -1,37 +1,48 @@
+#!/usr/bin/env python3
+import os
+
 import requests
 from requests.structures import CaseInsensitiveDict
 
-import os
-
-GITHUB_USER = os.envron.get("GITHUB_USER")
-GITHUB_REPO = os.envron.get("GITHUB_REPO")
-GITHUB_TAGGING_TOKEN = os.envron.get("GITHUB_TAGGING_TOKEN")
+GITHUB_USER = os.environ.get("GITHUB_USER")
+GITHUB_REPO = os.environ.get("GITHUB_REPO")
+GITHUB_TAGGING_TOKEN = os.environ.get("GITHUB_TAGGING_TOKEN")
 
 
-def release(package_name, version, github_user, github_repo, github_tagging_token):
-    """ Trigger the GitHub Release"""
-    print("release")
+def github_release(
+    package_name,
+    version,
+    github_user=GITHUB_USER,
+    github_repo=GITHUB_REPO,
+    github_tagging_token=GITHUB_TAGGING_TOKEN,
+):
+    """Trigger the GitHub Release to create artifacts and tags"""
+    print("Starting GitHub Release")
 
-    """ The release of GitHub artifacts and tagging"""
-
-
-    GITHUB_ENDPOINT = f"https://api.github.com/repos/{github_user}/{github_repo}/releases"
+    GITHUB_ENDPOINT = (
+        f"https://api.github.com/repos/{github_user}/{github_repo}/releases"
+    )
     PAYLOAD = {
-    "tag_name": f"{package_name}-{version}",  # kedro-datasets 0.0.1
-    "target_commitish": "main",
-    "name": f"{version}",
-    "body": f"Release {version}",
-    "draft": False,
-    "prerelease": False
-}
+        "tag_name": f"{package_name}-{version}",  # kedro-datasets 0.0.1
+        "target_commitish": "main",
+        "name": f"{version}",
+        "body": f"Release {version}",
+        "draft": False,
+        "prerelease": False,
+    }
 
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
     headers["Authorization"] = f"token {github_tagging_token}"
-    return
     resp = requests.post(GITHUB_ENDPOINT, headers=headers, json=PAYLOAD)
     print(resp.status_code)
+    print(resp.content)
     return resp
 
+
 if __name__ == "__main__":
-    res = release(PACKAGE_NAME, VERSION, GITHUB_USER, GITHUB_REPO, GITHUB_TAGGING_TOKEN)
+    import sys
+
+    package_name = sys.argv[0]
+    package_version = sys.argv[1]
+    res = github_release(package_name, package_version)

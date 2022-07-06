a0b5efef6206b9ae4141be85732d8caeba637e40
Nok Chan <nok.lam.chan@quantumblack.com>
2022-07-06 17:39:18 +0100

    trigger a proper release
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- kedro-datasets/kedro_datasets/__init__.py
+++ kedro-datasets/kedro_datasets/__init__.py
@@ -1,6 +1,6 @@
 """``kedro_datasets`` is where you can find all of Kedro's data connectors."""
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 
 # todo: remove this before merge Trigger CI with file changes

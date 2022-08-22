3bd525241a29bf9fff8f7b463ffc36c150d4c043
Deepyaman Datta <deepyaman.datta@utexas.edu>
2022-08-22 11:31:59 -0400

    Update setup.py

--- kedro-datasets/setup.py
+++ kedro-datasets/setup.py
@@ -23,12 +23,7 @@ with open(path.join(here, package_name, "__init__.py"), encoding="utf-8") as f:
 with open("requirements.txt", "r", encoding="utf-8") as f:
     requires = [x.strip() for x in f if x.strip()]
 
-# get test dependencies and installs
-with open("test_requirements.txt", "r", encoding="utf-8") as f:
-    test_requires = [x.strip() for x in f if x.strip() and not x.startswith("-r")]
-
-
-# Get the long description from the README file
+# get the long description from the README file
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     readme = f.read()
 

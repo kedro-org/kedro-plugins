afffbe2203ffa14ec5ecbafee8f7c8de4c5d4083
Deepyaman Datta <deepyaman.datta@utexas.edu>
2022-10-11 11:31:35 -0700

    Update kedro-docker/kedro_docker/plugin.py

--- kedro-docker/kedro_docker/plugin.py
+++ kedro-docker/kedro_docker/plugin.py
@@ -87,7 +87,7 @@ def _make_docker_args_option(**kwargs):
 
 
 @click.group(name="Docker")
-def commands():  # pylint: disable=missing-function-docstring
+def commands(): # pylint: disable=missing-function-docstring
     pass
 
 

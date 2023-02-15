454d58a4a2fbe516e45402b7852c774122e018f2
Yassine Alouini <yalouini@idmog.com>
2023-02-15 17:28:00 +0100

    Add pyodbc dependency.
    Signed-off-by: Yassine Alouini <yalouini@idmog.com>

--- kedro-datasets/setup.py
+++ kedro-datasets/setup.py
@@ -57,8 +57,7 @@ pandas_require = {
     "pandas.JSONDataSet": [PANDAS],
     "pandas.ParquetDataSet": [PANDAS, "pyarrow>=6.0"],
     "pandas.SQLTableDataSet": [PANDAS, "SQLAlchemy~=1.2"],
-    "pandas.SQLQueryDataSet": [PANDAS, "SQLAlchemy~=1.2"],
-    # TODO: Add the mssql dataset requirements here. Or maybe not needed?
+    "pandas.SQLQueryDataSet": [PANDAS, "SQLAlchemy~=1.2", "pyodbc~=4.0"],
     "pandas.XMLDataSet": [PANDAS, "lxml~=4.6"],
     "pandas.GenericDataSet": [PANDAS],
 }

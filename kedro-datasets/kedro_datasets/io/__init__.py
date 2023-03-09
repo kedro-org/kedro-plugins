bd1ebfdeade49acd2a6aa18b4fd4a58163d7a60d
brandonmeek <bpmeek.developer@gmail.com>
2023-03-09 16:21:42 -0600

    Added kedro/io/core.py kedro/io/__init__.py and kedro/utils.py from patch to keep history.

--- kedro-datasets/kedro_datasets/io/__init__.py
+++ kedro-datasets/kedro_datasets/io/__init__.py
@@ -3,7 +3,6 @@ number of data sets. At core of the library is ``AbstractDataSet``
 which allows implementation of various ``AbstractDataSet``s.
 """
 
-from .cached_dataset import CachedDataSet
 from .core import (
     AbstractDataSet,
     AbstractVersionedDataSet,
@@ -12,22 +11,12 @@ from .core import (
     DataSetNotFoundError,
     Version,
 )
-from .data_catalog import DataCatalog
-from .lambda_dataset import LambdaDataSet
-from .memory_dataset import MemoryDataSet
-from .partitioned_dataset import IncrementalDataSet, PartitionedDataSet
 
 __all__ = [
     "AbstractDataSet",
     "AbstractVersionedDataSet",
-    "CachedDataSet",
-    "DataCatalog",
     "DataSetAlreadyExistsError",
     "DataSetError",
     "DataSetNotFoundError",
-    "IncrementalDataSet",
-    "LambdaDataSet",
-    "MemoryDataSet",
-    "PartitionedDataSet",
     "Version",
 ]

064d3f43369da28f6ab2337e3bc6cdecab4f3d6d
huongg <huongg1409@gmail.com>
2023-03-21 21:14:53 +0000

    set defaul val for excel nrows
    Signed-off-by: huongg <huongg1409@gmail.com>

--- kedro-datasets/kedro_datasets/pandas/excel_dataset.py
+++ kedro-datasets/kedro_datasets/pandas/excel_dataset.py
@@ -259,7 +259,7 @@ class ExcelDataSet(
         filepath = get_filepath_str(self._filepath, self._protocol)
         self._fs.invalidate_cache(filepath)
 
-    def _preview(self, nrows) -> Dict:
+    def _preview(self, nrows: int = 40) -> Dict:
         # Create a copy so it doesn't contaminate the original dataset
         dataset_copy = self._copy()
         dataset_copy._load_args["nrows"] = nrows

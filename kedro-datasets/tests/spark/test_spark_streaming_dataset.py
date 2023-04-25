11c3888a9930b1d10795dc0d82e240975382c7ab
Tingting_Wan <tingting_wan@mckinsey.com>
2023-04-25 10:45:45 +0100

    formatting
    Signed-off-by: Tingting_Wan <tingting_wan@mckinsey.com>

--- kedro-datasets/tests/spark/test_spark_streaming_dataset.py
+++ kedro-datasets/tests/spark/test_spark_streaming_dataset.py
@@ -1,17 +1,14 @@
 import json
 import pytest
-import time
 from pyspark.sql import SparkSession
-from kedro_datasets.pandas import ParquetDataSet
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 from kedro.io.core import DataSetError
 from kedro_datasets.spark.spark_dataset import SparkDataSet
 from kedro_datasets.spark.spark_streaming_dataset import SparkStreamingDataSet
-from pyspark.sql.types import IntegerType, StringType, StructField, StructType
-
 
 
 def sample_schema(schema_path):
-    with open(schema_path, encoding='utf-8') as f:
+    with open(schema_path, encoding="utf-8") as f:
         try:
             return StructType.fromJson(json.loads(f.read()))
         except Exception as exc:
@@ -19,6 +16,7 @@ def sample_schema(schema_path):
                 f"Contents of 'schema.filepath' ({schema_path}) are invalid. "
                 f"Schema is required for streaming data load, Please provide a valid schema_path."
             ) from exc
+
 
 @pytest.fixture
 def sample_spark_streaming_df(tmp_path):
@@ -41,12 +39,15 @@ class TestStreamingDataSet:
         schema_path = (tmp_path / "test.json").as_posix()
 
         spark_json_ds = SparkDataSet(
-            filepath=filepath, file_format="json", save_args=[{"mode","overwrite"}]
+            filepath=filepath, file_format="json", save_args=[{"mode", "overwrite"}]
         )
         spark_json_ds.save(sample_spark_streaming_df)
 
-        streaming_ds = SparkStreamingDataSet(filepath=filepath, file_format="json",
-                                          load_args={"schema": {"filepath": schema_path}}).load()
+        streaming_ds = SparkStreamingDataSet(
+            filepath=filepath,
+            file_format="json",
+            load_args={"schema": {"filepath": schema_path}},
+        ).load()
         assert streaming_ds.isStreaming
         schema = sample_schema(schema_path)
         assert streaming_ds.schema == schema
@@ -58,20 +59,23 @@ class TestStreamingDataSet:
         checkpoint_path = (tmp_path / "checkpoint").as_posix()
 
         spark_json_ds = SparkDataSet(
-            filepath=filepath_json, file_format="json", save_args=[{"mode","overwrite"}]
+            filepath=filepath_json,
+            file_format="json",
+            save_args=[{"mode", "overwrite"}],
         )
         spark_json_ds.save(sample_spark_streaming_df)
-        loaded_with_streaming = SparkStreamingDataSet(filepath=filepath_json, file_format="json",
-                                          load_args={"schema": {"filepath": schema_path}}).load()
-
+        loaded_with_streaming = SparkStreamingDataSet(
+            filepath=filepath_json,
+            file_format="json",
+            load_args={"schema": {"filepath": schema_path}},
+        ).load()
 
         streaming_ds = SparkStreamingDataSet(
-            filepath=filepath_output, file_format="json",save_args={"checkpoint": checkpoint_path, "output_mode":"append"}
+            filepath=filepath_output,
+            file_format="json",
+            save_args={"checkpoint": checkpoint_path, "output_mode": "append"},
         )
         assert not streaming_ds._exists(schema_path)
 
         streaming_ds.save(loaded_with_streaming)
         assert streaming_ds._exists(schema_path)
-
-
-

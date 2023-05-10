97055cedb9d1f588fb868951edaf127d708de071
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-05-10 23:29:16 +0100

    new

--- kedro-datasets/kedro_datasets/__init__.py
+++ kedro-datasets/kedro_datasets/__init__.py
@@ -1,3 +1,3 @@
-"""``kedro_datasets`` is where you can find all of Kedro's data connectors. [to-delete]"""
+"""``kedro_datasets`` is where you can find all of Kedro's data connectors."""
 
-__version__ = "1.2.0"
+__version__ = "2.2.0"

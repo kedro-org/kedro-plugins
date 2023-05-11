625c86a2a8de05ac3b99e73afd0806f26acf7691
jmcdonnell <jmcdonnell@fieldbox.ai>
2023-05-11 10:24:20 +0200

    [FIX] remove support for delete method
    Signed-off-by: jmcdonnell <jmcdonnell@fieldbox.ai>

--- kedro-datasets/kedro_datasets/api/api_dataset.py
+++ kedro-datasets/kedro_datasets/api/api_dataset.py
@@ -25,9 +25,15 @@ class APIDataSet(AbstractDataSet[None, requests.Response]):
     .. code-block:: yaml
 
         usda:
-          type: api.APIDataSet url: https://quickstats.nass.usda.gov params:
-            key: SOME_TOKEN, format: JSON, commodity_desc: CORN, statisticcat_des: YIELD,
-            agg_level_desc: STATE, year: 2000
+          type: api.APIDataSet 
+          url: https://quickstats.nass.usda.gov 
+        params:
+            key: SOME_TOKEN, 
+            format: JSON, 
+            commodity_desc: CORN, 
+            statisticcat_des: YIELD,
+            agg_level_desc: STATE, 
+            year: 2000
 
     Example usage for the `Python API <https://kedro.readthedocs.io/en/stable/data/\
     data_catalog.html#use-the-data-catalog-with-the-code-api>`_: ::
@@ -51,7 +57,7 @@ class APIDataSet(AbstractDataSet[None, requests.Response]):
         >>> )
         >>> data = data_set.load()
 
-    ``APIDataSet`` can also be used to save some output on some remote server using
+    ``APIDataSet`` can also be used to save output on a remote server using
     HTTP(S) methods.
 
         >>> import pandas as pd
@@ -65,11 +71,11 @@ class APIDataSet(AbstractDataSet[None, requests.Response]):
                 save_args = {"method":"POST",
                             "chunk_size":1}
         )
-    On initialization, we can specify all the necessary parameters in the save args
-    dictionary. The default HTTP(S) method is POST but all other methods are supported.
+    On initialisation, we can specify all the necessary parameters in the save args
+    dictionary. The default HTTP(S) method is POST but PUT is also supported.
     Two important parameters to keep in mind are timeout and chunk_size. ``Timeout``
     defines how long  our program waits for a response after a request. ``Chunk_size``, is
-    only used if the input of save method is a list. It will, divide the request into
+    only used if the input of save method is a list. It will divide the request into
     chunks of size ``chunk_size``. For example, here we will send two requests each
     containing one row of our example DataFrame.
 
@@ -104,12 +110,32 @@ class APIDataSet(AbstractDataSet[None, requests.Response]):
 
         Args:
             url: The API URL endpoint.
-            method: The Method of the request, GET, POST, PUT, DELETE, HEAD, etc...
+            method: The Method of the request, GET, POST, PUT
             load_args: Additional parameters to be fed to requests.request.
                 https://requests.readthedocs.io/en/latest/api/#requests.request
             credentials: Allows specifying secrets in credentials.yml.
                 Expected format is ``('login', 'password')`` if given as a tuple or list.
                 An ``AuthBase`` instance can be provided for more complex cases.
+            requests
+                https://requests.readthedocs.io/en/latest/user/quickstart/#more-complicated-post-requests
+            params: The url parameters of the API.
+                https://requests.readthedocs.io/en/latest/user/quickstart/#passing-parameters-in-urls
+            headers: The HTTP headers.
+                https://requests.readthedocs.io/en/latest/user/quickstart/#custom-headers
+            auth: Anything ``requests`` accepts. Normally it's either ``('login',
+            'password')``,
+                or ``AuthBase``, ``HTTPBasicAuth`` instance for more complex cases. Any
+                iterable will be cast to a tuple.
+            json: The request payload, used for POST, PUT, etc requests, passed in
+                to the json kwarg in the requests object.
+                https://requests.readthedocs.io/en/latest/user/quickstart/#more-complicated-post-requests
+            timeout: The wait time in seconds for a response, defaults to 1 minute.
+                https://requests.readthedocs.io/en/latest/user/quickstart/#timeouts
+            credentials: same as ``auth``. Allows specifying ``auth`` secrets in
+                credentials.yml.
+            save_args: Options for saving data on server. Includes all parameters used
+                during load method. Adds an optional parameter, ``chunk_size`` which determines the
+                size of the package sent at each request.
         Raises:
             ValueError: if both ``auth`` in ``load_args`` and ``credentials`` are specified.
         """
@@ -118,7 +144,18 @@ class APIDataSet(AbstractDataSet[None, requests.Response]):
         self._load_args = load_args or {}
         self._load_args_auth = self._load_args.pop("auth", None)
 
-        if credentials is not None and self._load_args_auth is not None:
+        # PUT, POST, DELETE means save
+        elif method in ["PUT", "POST"]:
+            self._params = deepcopy(self.DEFAULT_SAVE_ARGS)
+            if save_args is not None:
+                self._params.update(save_args)
+            self._chunk_size = self._params.pop("chunk_size", 1)
+        else:
+            raise ValueError("Only GET, POST and PUT methods are supported")
+
+        self._param_auth = self._params.pop("auth", None)
+
+        if credentials is not None and self._param_auth is not None:
             raise ValueError("Cannot specify both auth and credentials.")
 
         self._auth = credentials or self._load_args_auth
@@ -135,6 +172,18 @@ class APIDataSet(AbstractDataSet[None, requests.Response]):
             "auth": self._convert_type(self._auth),
             **self._load_args,
         }
+        self.chunk_size = chunk_size
+
+    @staticmethod
+    def _convert_type(value: Any):
+        """
+        From the Data Catalog, iterables are provided as Lists.
+        However, for some parameters in the Python requests library,
+        only Tuples are allowed.
+        """
+        if isinstance(value, List):
+            return tuple(value)
+        return value
 
     @staticmethod
     def _convert_type(value: Any):
--- kedro-datasets/tests/api/test_api_dataset.py
+++ kedro-datasets/tests/api/test_api_dataset.py
@@ -10,6 +10,7 @@ from requests.auth import HTTPBasicAuth
 from kedro_datasets.api import APIDataSet
 
 POSSIBLE_METHODS = ["GET", "OPTIONS", "HEAD", "POST", "PUT", "PATCH", "DELETE"]
+SAVE_METHODS = ["POST", "PUT"]
 
 TEST_URL = "http://example.com/api/test"
 TEST_TEXT_RESPONSE_DATA = "This is a response."
@@ -26,8 +27,17 @@ TEST_SAVE_DATA = [json.dumps({"key1": "info1", "key2": "info2"})]
 class TestAPIDataSet:
     @pytest.mark.parametrize("method", POSSIBLE_METHODS)
     def test_request_method(self, requests_mock, method):
-        api_data_set = APIDataSet(url=TEST_URL, method=method)
-        requests_mock.register_uri(method, TEST_URL, text=TEST_TEXT_RESPONSE_DATA)
+        if method in ["OPTIONS", "HEAD", "PATCH", "DELETE"]:
+            with pytest.raises(
+                ValueError,
+                match="Only GET, POST and PUT methods are supported",
+            ):
+                APIDataSet(url=TEST_URL, method=method)
+
+        else:
+            api_data_set = APIDataSet(url=TEST_URL, method=method)
+
+            requests_mock.register_uri(method, TEST_URL, text=TEST_TEXT_RESPONSE_DATA)
 
         response = api_data_set.load()
         assert response.text == TEST_TEXT_RESPONSE_DATA
@@ -343,6 +353,104 @@ class TestAPIDataSet:
         """
         Saving is disabled on the data set.
         """
-        api_data_set = APIDataSet(url=TEST_URL, method=TEST_METHOD)
-        with pytest.raises(DataSetError, match="is a read only data set type"):
-            api_data_set.save({})
+        if method in ["PUT", "POST"]:
+            api_data_set = APIDataSet(
+                url=TEST_URL,
+                method=method,
+                save_args={"params": TEST_PARAMS, "headers": TEST_HEADERS},
+            )
+            requests_mocker.register_uri(
+                method,
+                TEST_URL_WITH_PARAMS,
+                headers=TEST_HEADERS,
+                status_code=requests.codes.ok,
+            )
+            response = api_data_set._save(TEST_SAVE_DATA)
+
+            assert isinstance(response, requests.Response)
+        elif method == "GET":
+            api_data_set = APIDataSet(
+                url=TEST_URL,
+                method=method,
+                save_args={"params": TEST_PARAMS, "headers": TEST_HEADERS},
+            )
+            with pytest.raises(
+                DataSetError, match="Use PUT, POST or DELETE methods for save"
+            ):
+                api_data_set._save(TEST_SAVE_DATA)
+        else:
+            with pytest.raises(
+                ValueError,
+                match="Only GET, POST and PUT methods are supported",
+            ):
+                APIDataSet(url=TEST_URL, method=method)
+
+    @pytest.mark.parametrize("save_methods", SAVE_METHODS)
+    def test_successful_save_with_json(self, requests_mocker, save_methods):
+        """
+        When we want to save with json parameters
+        Given an APIDataSet class
+        Then check we get a response
+        """
+        api_data_set = APIDataSet(
+            url=TEST_URL,
+            method=save_methods,
+            save_args={"json": TEST_JSON_RESPONSE_DATA, "headers": TEST_HEADERS},
+        )
+        requests_mocker.register_uri(
+            save_methods,
+            TEST_URL,
+            headers=TEST_HEADERS,
+            text=json.dumps(TEST_JSON_RESPONSE_DATA),
+        )
+        response_list = api_data_set._save(TEST_SAVE_DATA)
+
+        assert isinstance(response_list, requests.Response)
+
+        response_dict = api_data_set._save({"item1": "key1"})
+        assert isinstance(response_dict, requests.Response)
+
+        response_json = api_data_set._save(TEST_SAVE_DATA[0])
+        assert isinstance(response_json, requests.Response)
+
+    @pytest.mark.parametrize("save_methods", SAVE_METHODS)
+    def test_save_http_error(self, requests_mocker, save_methods):
+        api_data_set = APIDataSet(
+            url=TEST_URL,
+            method=save_methods,
+            save_args={"params": TEST_PARAMS, "headers": TEST_HEADERS, "chunk_size": 2},
+        )
+        requests_mocker.register_uri(
+            save_methods,
+            TEST_URL_WITH_PARAMS,
+            headers=TEST_HEADERS,
+            text="Nope, not found",
+            status_code=requests.codes.FORBIDDEN,
+        )
+
+        with pytest.raises(DataSetError, match="Failed to send data"):
+            api_data_set.save(TEST_SAVE_DATA)
+
+        with pytest.raises(DataSetError, match="Failed to send data"):
+            api_data_set.save(TEST_SAVE_DATA[0])
+
+    @pytest.mark.parametrize("save_methods", SAVE_METHODS)
+    def test_save_socket_error(self, requests_mocker, save_methods):
+        api_data_set = APIDataSet(
+            url=TEST_URL,
+            method=save_methods,
+            save_args={"params": TEST_PARAMS, "headers": TEST_HEADERS},
+        )
+        requests_mocker.register_uri(
+            save_methods, TEST_URL_WITH_PARAMS, exc=socket.error
+        )
+
+        with pytest.raises(
+            DataSetError, match="Failed to connect to the remote server"
+        ):
+            api_data_set.save(TEST_SAVE_DATA)
+
+        with pytest.raises(
+            DataSetError, match="Failed to connect to the remote server"
+        ):
+            api_data_set.save(TEST_SAVE_DATA[0])

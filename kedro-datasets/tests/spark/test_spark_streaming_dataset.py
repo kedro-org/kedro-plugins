37e66e8c603b48272ec1771181e6162505fc2a53
Tom Kurian <tom_kurian@mckinsey.com>
2023-05-16 21:14:44 +0100

    add additional test cases
    Signed-off-by: Tom Kurian <tom_kurian@mckinsey.com>

--- kedro-datasets/tests/spark/test_spark_streaming_dataset.py
+++ kedro-datasets/tests/spark/test_spark_streaming_dataset.py
@@ -1,7 +1,9 @@
 import json
 
+import boto3
 import pytest
 from kedro.io.core import DataSetError
+from moto import mock_s3
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 from pyspark.sql.utils import AnalysisException
@@ -10,9 +12,12 @@ from kedro_datasets.spark.spark_dataset import SparkDataSet
 from kedro_datasets.spark.spark_streaming_dataset import SparkStreamingDataSet
 
 SCHEMA_FILE_NAME = "schema.json"
+BUCKET_NAME = "test_bucket"
+AWS_CREDENTIALS = {"key": "FAKE_ACCESS_KEY", "secret": "FAKE_SECRET_KEY"}
 
 
 def sample_schema(schema_path):
+    """read the schema file from json path"""
     with open(schema_path, encoding="utf-8") as f:
         try:
             return StructType.fromJson(json.loads(f.read()))
@@ -24,18 +29,51 @@ def sample_schema(schema_path):
 
 
 @pytest.fixture
-def sample_spark_streaming_df(tmp_path):
-    schema = StructType(
+def sample_spark_df_schema() -> StructType:
+    """Spark Dataframe schema"""
+    return StructType(
         [
             StructField("sku", StringType(), True),
             StructField("new_stock", IntegerType(), True),
         ]
     )
+
+
+@pytest.fixture
+def sample_spark_streaming_df(tmp_path, sample_spark_df_schema):
+    """Create s sample dataframe for streaming"""
     data = [("0001", 2), ("0001", 7), ("0002", 4)]
     schema_path = (tmp_path / SCHEMA_FILE_NAME).as_posix()
     with open(schema_path, "w", encoding="utf-8") as f:
-        json.dump(schema.jsonValue(), f)
-    return SparkSession.builder.getOrCreate().createDataFrame(data, schema)
+        json.dump(sample_spark_df_schema.jsonValue(), f)
+    return SparkSession.builder.getOrCreate().createDataFrame(
+        data, sample_spark_df_schema
+    )
+
+
+@pytest.fixture
+def mocked_s3_bucket():
+    """Create a bucket for testing using moto."""
+    with mock_s3():
+        conn = boto3.client(
+            "s3",
+            aws_access_key_id="fake_access_key",
+            aws_secret_access_key="fake_secret_key",
+        )
+        conn.create_bucket(Bucket=BUCKET_NAME)
+        yield conn
+
+
+@pytest.fixture
+def mocked_s3_schema(tmp_path, mocked_s3_bucket, sample_spark_df_schema: StructType):
+    """Creates schema file and adds it to mocked S3 bucket."""
+    temporary_path = tmp_path / SCHEMA_FILE_NAME
+    temporary_path.write_text(sample_spark_df_schema.json(), encoding="utf-8")
+
+    mocked_s3_bucket.put_object(
+        Bucket=BUCKET_NAME, Key=SCHEMA_FILE_NAME, Body=temporary_path.read_bytes()
+    )
+    return mocked_s3_bucket
 
 
 class TestStreamingDataSet:
@@ -53,6 +91,33 @@ class TestStreamingDataSet:
             file_format="json",
             load_args={"schema": {"filepath": schema_path}},
         ).load()
+        assert streaming_ds.isStreaming
+        schema = sample_schema(schema_path)
+        assert streaming_ds.schema == schema
+
+    @pytest.mark.usefixtures("mocked_s3_schema")
+    def test_load_options_schema_path_with_credentials(
+        self, tmp_path, sample_spark_streaming_df
+    ):
+        filepath = (tmp_path / "test_streams").as_posix()
+        schema_path = (tmp_path / SCHEMA_FILE_NAME).as_posix()
+
+        spark_json_ds = SparkDataSet(
+            filepath=filepath, file_format="json", save_args=[{"mode", "overwrite"}]
+        )
+        spark_json_ds.save(sample_spark_streaming_df)
+
+        streaming_ds = SparkStreamingDataSet(
+            filepath=filepath,
+            file_format="json",
+            load_args={
+                "schema": {
+                    "filepath": f"s3://{BUCKET_NAME}/{SCHEMA_FILE_NAME}",
+                    "credentials": AWS_CREDENTIALS,
+                }
+            },
+        ).load()
+
         assert streaming_ds.isStreaming
         schema = sample_schema(schema_path)
         assert streaming_ds.schema == schema

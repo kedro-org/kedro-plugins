cd7d4b62613f3e3534355d303479eeb0c7f83c6b
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-05-17 18:02:39 +0100

    fix script to not append continuously

--- tools/github_actions/github_actions_release.py
+++ tools/github_actions/github_actions_release.py
@@ -34,16 +34,21 @@ def check_no_version_pypi(pypi_endpoint, package_name, package_version):
 if __name__ == "__main__":
     """Check if a package needs to be released"""
     base_path = Path()
+    new_release = "false"
+    package_name = None
+    package_version = None
+
     for package_path in PACKAGE_PATHS:
         package_name, _ = package_path.split("/")
         package_version = get_package_version(base_path, package_path)
         pypi_endpoint = f"https://pypi.org/pypi/{package_name}/{package_version}/json/"
-        env_file = os.getenv('GITHUB_ENV')
 
         if check_no_version_pypi(pypi_endpoint, package_name, package_version):
-            with open(env_file, "a") as env_file:
-                env_file.write(f"NEW_RELEASE=true\nPACKAGE_NAME={package_name}\nPACKAGE_VERSION={package_version}\n")
+            new_release = "true"
             break
-        else:
-            with open(env_file, "a") as env_file:
-                env_file.write("new_release=false")
+
+    env_file = os.getenv('GITHUB_ENV')
+    with open(env_file, "a") as env_file:
+        env_file.write(f"NEW_RELEASE={new_release}\n")
+        if new_release == "true":
+            env_file.write(f"PACKAGE_NAME={package_name}\nPACKAGE_VERSION={package_version}\n")

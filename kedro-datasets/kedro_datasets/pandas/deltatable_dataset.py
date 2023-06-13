ac4b6de5207a3a12634c5759c1c132fd3a01ef74
Afaque Ahmad <linkedafaque@gmail.com>
2023-06-13 13:11:40 +0800

    chore: cleanup and added property methods
    Signed-off-by: Afaque Ahmad <linkedafaque@gmail.com>

--- kedro-datasets/kedro_datasets/pandas/deltatable_dataset.py
+++ kedro-datasets/kedro_datasets/pandas/deltatable_dataset.py
@@ -1,94 +1,89 @@
-import logging
 from copy import deepcopy
-from typing import Any, Dict, Tuple
+from typing import Any, Dict
 
-import numpy as np
 import pandas as pd
 from deltalake import DeltaTable
 from deltalake.writer import write_deltalake
-from kedro.io.core import AbstractDataSet
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
-def _split_filepath(filepath: str) -> Tuple[str, str]:
-    split_ = filepath.split("://", 1)
-    if len(split_) == 2:
-        return split_[0] + "://", split_[1]
-    return "", split_[0]
+from kedro.io.core import AbstractDataSet, DataSetError
 
 
 class DeltaTableDataSet(AbstractDataSet):
     DEFAULT_WRITE_MODE = "overwrite"
+    ACCEPTED_WRITE_MODES = (
+        "overwrite",
+        "append"
+    )
+
+    DEFAULT_LOAD_ARGS: Dict[str, Any] = {}
+    DEFAULT_SAVE_ARGS: Dict[str, Any] = {
+        "mode": DEFAULT_WRITE_MODE
+    }
 
     def __init__(  # pylint: disable=too-many-arguments
         self,
         filepath: str,
         load_args: Dict[str, Any] = None,
         save_args: Dict[str, Any] = None,
+        version: int = None,
         credentials: Dict[str, Any] = None,
+        fs_args: Dict[str, Any] = None,
+        metadata: Dict[str, Any] = None
     ) -> None:
-        self._read_path = filepath
+        self._filepath = filepath
+        self._fs_args = deepcopy(fs_args) or {}
         self._credentials = deepcopy(credentials) or {}
+        self._version = version
 
-        fs_prefix, filepath = _split_filepath(filepath)
-        split = filepath.split("/", 1)
-        self._write_path = (
-            "abfs://"
-            + split[0]
-            + "@"
-            + self._credentials["account_name"]
-            + ".dfs.core.windows.net/"
-            + split[1]
+        self.metadata = metadata
+
+        self._load_args = deepcopy(self.DEFAULT_LOAD_ARGS)
+        if load_args:
+            self._load_args.update(load_args)
+
+        self._save_args = deepcopy(self.DEFAULT_SAVE_ARGS)
+        if save_args:
+            self._save_args.update(save_args)
+
+        write_mode = self._save_args.get("mode", None)
+        if write_mode not in self.ACCEPTED_WRITE_MODES:
+            raise DataSetError(
+                f"Write mode {write_mode} is not supported, "
+                f"Please use any of the following accepted modes "
+                f"{self.ACCEPTED_WRITE_MODES}"
+            )
+
+    @property
+    def fs_args(self) -> Dict[str, Any]:
+        fs_args = deepcopy(self._fs_args)
+        fs_args.update(self._credentials)
+        return fs_args
+
+    def delta_table(self) -> DeltaTable:
+        delta_table = DeltaTable(
+            self._filepath,
+            storage_options=self.fs_args,
+            version=self._version
         )
-
-        self._storage_options = {
-            "account_name": self._credentials["account_name"],
-            "account_key": self._credentials["account_key"],
-        }
-
-        self._load_args = deepcopy(load_args) or {}
-        self._save_args = deepcopy(save_args) or {}
-
-        self._write_mode = self._save_args.get(
-            "mode",
-            DeltaTableDataSet.DEFAULT_WRITE_MODE,
-        )
-
-        self._load_version = self._load_args.get("version", None)
-        if self._load_version:
-            self._load_version = int(self._load_version)
-
-        self._delta_table = DeltaTable(
-            self._read_path,
-            storage_options=self._storage_options,
-            version=self._load_version,
-        )
+        return delta_table
 
     def get_loaded_version(self) -> int:
-        return self._delta_table.version()
+        return self.delta_table().version()
 
     def _load(self) -> pd.DataFrame:
-        df = self._delta_table.to_pandas()
-        logger.info(f"Loaded {len(df)} rows from {self._read_path}")
-        return df
+        return self.delta_table().to_pandas()
 
     def _save(self, data: pd.DataFrame) -> None:
         write_deltalake(
-            self._write_path,
+            self._filepath,
             data,
-            storage_options=self._storage_options,
-            mode=self._write_mode,
+            storage_options=self.fs_args,
+            **self._save_args,
         )
-        logger.info(f"Saved {len(data)} rows to {self._write_path}")
 
     def _describe(self) -> Dict[str, Any]:
         return {
-            "filepath": self._read_path,
-            "versions": int(len(self._delta_table.history())) - 1,
-            "load_version": self.get_loaded_version(),
-            "read_path": self._read_path,
-            "write_path": self._write_path,
-            "write_mode": self._write_mode,
+            "filepath": self._filepath,
+            "load_args": self._load_args,
+            "save_args": self._save_args,
+            "version": self._version,
         }

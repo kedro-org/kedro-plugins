a4765b2d278d80afc35e995b5e494817076cb3f0
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-06-30 15:40:48 +0100

    Remove unnecessary type hints from doc string
    Signed-off-by: Jannic Holzer <jannic.holzer@quantumblack.com>

--- kedro-datasets/kedro_datasets/databricks/managed_table_dataset.py
+++ kedro-datasets/kedro_datasets/databricks/managed_table_dataset.py
@@ -212,28 +212,28 @@ class ManagedTableDataSet(AbstractVersionedDataSet):
         """Creates a new instance of ``ManagedTableDataSet``
 
         Args:
-            table (str): the name of the table
-            catalog (str, optional): the name of the catalog in Unity.
+            table: the name of the table
+            catalog: the name of the catalog in Unity.
              Defaults to None.
-            database (str, optional): the name of the database.
+            database: the name of the database.
              (also referred to as schema). Defaults to "default".
-            write_mode (str, optional): the mode to write the data into the table.
+            write_mode: the mode to write the data into the table.
              Options are:["overwrite", "append", "upsert"].
              "upsert" mode requires primary_key field to be populated.
              Defaults to "overwrite".
-            dataframe_type (str, optional): "pandas" or "spark" dataframe.
+            dataframe_type: "pandas" or "spark" dataframe.
              Defaults to "spark".
-            primary_key (Union[str, List[str]], optional): the primary key of the table.
+            primary_key: the primary key of the table.
              Can be in the form of a list. Defaults to None.
-            version (Version, optional): kedro.io.core.Version instance to load the data.
+            version: kedro.io.core.Version instance to load the data.
              Defaults to None.
-            schema (Dict[str, Any], optional): the schema of the table in JSON form.
+            schema: the schema of the table in JSON form.
              Dataframes will be truncated to match the schema if provided.
              Used by the hooks to create the table if the schema is provided
              Defaults to None.
-            partition_columns (List[str], optional): the columns to use for partitioning the table.
+            partition_columns: the columns to use for partitioning the table.
              Used by the hooks. Defaults to None.
-            owner_group (str, optional): if table access control is enabled in your workspace,
+            owner_group: if table access control is enabled in your workspace,
              specifying owner_group will transfer ownership of the table and database to
              this owner. All databases should have the same owner_group. Defaults to None.
         Raises:

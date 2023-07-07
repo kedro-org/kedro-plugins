a14be09668310c93fcd9ce9a69b217a62466ef3e
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-07-07 18:56:32 +0100

    Make _io tests restore old values
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- kedro-datasets/tests/test_io.py
+++ kedro-datasets/tests/test_io.py
@@ -1,82 +1,115 @@
 import importlib
 
+import pytest
 
-def test_io_dataset_error_forwards_compatible(mocker):
-    mocked_module_name = "MockedDatasetError"
 
+@pytest.fixture()
+def patch_kedro_io_core_dataset_error():
     kedro_io_core = importlib.import_module("kedro.io.core")
 
-    # Override the *new* class, or create it if it doesn't exist
-    kedro_io_core.DatasetError = mocker.MagicMock()
-    kedro_io_core.DatasetError.__name__ = mocked_module_name
-
-    kedro_datasets_io = importlib.import_module("kedro_datasets._io")
-    importlib.reload(kedro_datasets_io)
-
-    assert kedro_datasets_io.DatasetError.__name__ == mocked_module_name
-
-
-def test_io_dataset_error_backwards_compatible(mocker):
-    mocked_module_name = "MockedDatasetError"
-
-    kedro_io_core = importlib.import_module("kedro.io.core")
-
-    # Delete the *new* class if it exists
     try:
+        dataset_error_new = kedro_io_core.DatasetError
         del kedro_io_core.DatasetError
+        dataset_error_old = None
     except:
-        pass
+        dataset_error_new = None
+        dataset_error_old = kedro_io_core.DataSetError
+        del kedro_io_core.DataSetError
 
-    # Override the *old* class, or create it if it doesn't exist
-    kedro_io_core.DataSetError = mocker.MagicMock()
-    kedro_io_core.DataSetError.__name__ = mocked_module_name
+    yield kedro_io_core
 
-    kedro_datasets_io = importlib.import_module("kedro_datasets._io")
-    importlib.reload(kedro_datasets_io)
+    # Restore
+    if dataset_error_new:
+        kedro_io_core.DatasetError = dataset_error_new
+        try:
+            del kedro_io_core.DataSetError
+        except:
+            pass
+    else:
+        try:
+            del kedro_io_core.DatasetError
+        except:
+            pass
+        kedro_io_core.DataSetError = dataset_error_old
 
-    assert kedro_datasets_io.DatasetError.__name__ == mocked_module_name
 
-
-def test_io_abstract_dataset_forwards_compatible(mocker):
-    mocked_module_name_base = "MockedAbstractDataset"
-    mocked_module_name_versioned = "MockedAbstractVersionedDataset"
-
+@pytest.fixture()
+def patch_kedro_io_core_abstract_dataset():
     kedro_io_core = importlib.import_module("kedro.io.core")
 
-    # Override the *new* classes, or create them if them don't exist
-    kedro_io_core.AbstractDataset = mocker.MagicMock()
-    kedro_io_core.AbstractDataset.__name__ = mocked_module_name_base
-    kedro_io_core.AbstractVersionedDataset = mocker.MagicMock()
-    kedro_io_core.AbstractVersionedDataset.__name__ = mocked_module_name_versioned
-
-    kedro_datasets_io = importlib.import_module("kedro_datasets._io")
-    importlib.reload(kedro_datasets_io)
-
-    assert kedro_datasets_io.AbstractDataset.__name__ == mocked_module_name_base
-    assert (
-        kedro_datasets_io.AbstractVersionedDataset.__name__
-        == mocked_module_name_versioned
-    )
-
-
-def test_io_abstract_dataset_backwards_compatible(mocker):
-    mocked_module_name_base = "MockedAbstractDataset"
-    mocked_module_name_versioned = "MockedAbstractVersionedDataset"
-
-    kedro_io_core = importlib.import_module("kedro.io.core")
-
-    # Delete the *new* classes if they exist
     try:
+        abstract_dataset_new = kedro_io_core.AbstractDataset
+        abstract_versioned_dataset_new = kedro_io_core.AbstractVersionedDataset
         del kedro_io_core.AbstractDataset
         del kedro_io_core.AbstractVersionedDataset
+        abstract_dataset_old = None
+        abstract_versioned_dataset_old = None
     except:
-        pass
+        abstract_dataset_new = None
+        abstract_versioned_dataset_new = None
+        abstract_dataset_old = kedro_io_core.AbstractDataSet
+        abstract_versioned_dataset_old = kedro_io_core.AbstractVersionedDataSet
+        del kedro_io_core.AbstractDataSet
+        del kedro_io_core.AbstractVersionedDataSet
 
-    # Override the *old* class, or create it if it doesn't exist
-    kedro_io_core.AbstractDataSet = mocker.MagicMock()
-    kedro_io_core.AbstractDataSet.__name__ = mocked_module_name_base
-    kedro_io_core.AbstractVersionedDataSet = mocker.MagicMock()
-    kedro_io_core.AbstractVersionedDataSet.__name__ = mocked_module_name_versioned
+    yield kedro_io_core
+
+    # Restore
+    if abstract_dataset_new:
+        kedro_io_core.AbstractDataset = abstract_dataset_new
+        kedro_io_core.AbstractVersionedDataset = abstract_versioned_dataset_new
+        try:
+            del kedro_io_core.AbstractDataSet
+            del kedro_io_core.AbstractVersionedDataSet
+        except:
+            pass
+    else:
+        try:
+            del kedro_io_core.AbstractDataset
+            del kedro_io_core.AbstractVersionedDataset
+        except:
+            pass
+        kedro_io_core.AbstractDataSet = abstract_dataset_old
+        kedro_io_core.AbstractVersionedDataSet = abstract_versioned_dataset_old
+
+
+@pytest.mark.parametrize("class_name", ["DatasetError", "DataSetError"])
+def test_io_dataset_error_compatible(
+    class_name, mocker, patch_kedro_io_core_dataset_error
+):
+    mocked_name = "MockedDatasetError"
+
+    mocked_object = mocker.MagicMock()
+    mocked_object.__name__ = mocked_name
+
+    setattr(patch_kedro_io_core_dataset_error, class_name, mocked_object)
+
+    kedro_datasets_io = importlib.import_module("kedro_datasets._io")
+    importlib.reload(kedro_datasets_io)
+
+    assert kedro_datasets_io.DatasetError.__name__ == mocked_name
+
+
+@pytest.mark.parametrize(
+    "class_names",
+    [
+        ("AbstractDataset", "AbstractVersionedDataset"),
+        ("AbstractDataSet", "AbstractVersionedDataSet"),
+    ],
+)
+def test_io_abstract_dataset_forwards_compatible(
+    class_names, mocker, patch_kedro_io_core_abstract_dataset
+):
+    mocked_module_name_base = "MockedAbstractDataset"
+    mocked_module_name_versioned = "MockedAbstractVersionedDataset"
+
+    # Override the *new* classes, or create them if they don't exist
+    for class_name, mocked_name in zip(
+        class_names, (mocked_module_name_base, mocked_module_name_versioned)
+    ):
+        mocked_object = mocker.MagicMock()
+        mocked_object.__name__ = mocked_name
+        setattr(patch_kedro_io_core_abstract_dataset, class_name, mocked_object)
 
     kedro_datasets_io = importlib.import_module("kedro_datasets._io")
     importlib.reload(kedro_datasets_io)

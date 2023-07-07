1823e4cc6d3f82c136ae4d1a499e58138726e600
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-07-07 14:19:24 +0200

    Typo
    Co-authored-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- kedro-datasets/kedro_datasets/_io.py
+++ kedro-datasets/kedro_datasets/_io.py
@@ -13,7 +13,7 @@ try:
     # kedro 0.18.12 onwards
     from kedro.io.core import AbstractDataset, AbstractVersionedDataset
 except ImportError:
-    # older versions:
+    # older versions
     from kedro.io.core import AbstractDataSet as AbstractDataset
     from kedro.io.core import AbstractVersionedDataSet as AbstractVersionedDataset
 

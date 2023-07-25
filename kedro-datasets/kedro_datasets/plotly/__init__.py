587b2c2d6ec3433eef6182c0955960f4c1e56c02
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-07-25 03:19:53 -0500

    perf(datasets): lazily load datasets in init files (plotly)
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- kedro-datasets/kedro_datasets/plotly/__init__.py
+++ kedro-datasets/kedro_datasets/plotly/__init__.py
@@ -1,11 +1,14 @@
 """``AbstractDataSet`` implementations to load/save a plotly figure from/to a JSON
 file."""
+from typing import Any
 
-__all__ = ["PlotlyDataSet", "JSONDataSet"]
+import lazy_loader as lazy
 
-from contextlib import suppress
+# https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
+JSONDataSet: Any
+PlotlyDataSet: Any
 
-with suppress(ImportError):
-    from .plotly_dataset import PlotlyDataSet
-with suppress(ImportError):
-    from .json_dataset import JSONDataSet
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__,
+    submod_attrs={"json_dataset": ["JSONDataSet"], "plotly_dataset": ["PlotlyDataSet"]},
+)

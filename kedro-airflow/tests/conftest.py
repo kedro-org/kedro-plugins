2ad06e15ed055620b0d25e112158a05ea2d17876
Simon Brugman <sbrugman@users.noreply.github.com>
2023-08-01 12:27:37 +0200

    feat: kedro-airflow DAG kwarg configuration (#233)
    * feature: kedro-airflow DAG kwarg configuration
    
    Signed-off-by: Simon Brugman <sfbbrugman@gmail.com>
    
    * feat: `kedro-airflow` DAG kwarg configuration
    
    Signed-off-by: Simon Brugman <sfbbrugman@gmail.com>
    
    * Consistent use of the __future__ annotations
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Apply suggestions from code review
    
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    
    Co-authored-by: Merel Theisen <49397448+merelcht@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Simon Brugman <sfbbrugman@gmail.com>
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    Co-authored-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    Co-authored-by: Nok <nok.lam.chan@quantumblack.com>
    Co-authored-by: Ankita Katiyar <110245118+ankatiyar@users.noreply.github.com>
    Co-authored-by: Merel Theisen <49397448+merelcht@users.noreply.github.com>

--- kedro-airflow/tests/conftest.py
+++ kedro-airflow/tests/conftest.py
@@ -4,16 +4,21 @@ this directory. You don't need to import the fixtures as pytest will
 discover them automatically. More info here:
 https://docs.pytest.org/en/latest/fixture.html
 """
+from __future__ import annotations
+
+import os
 from pathlib import Path
 from shutil import copyfile
 
 from click.testing import CliRunner
+from cookiecutter.main import cookiecutter
 from kedro import __version__ as kedro_version
+from kedro.framework.cli.starters import TEMPLATE_PATH
 from kedro.framework.startup import ProjectMetadata
 from pytest import fixture
 
 
-@fixture(name="cli_runner")
+@fixture(name="cli_runner", scope="session")
 def cli_runner():
     runner = CliRunner()
     cwd = Path.cwd()
@@ -23,10 +28,79 @@ def cli_runner():
         yield runner
 
 
-@fixture
-def metadata(cli_runner):  # pylint: disable=unused-argument
+def _create_kedro_settings_py(file_name: Path, patterns: list[str]):
+    patterns = ", ".join([f'"{p}"' for p in patterns])
+    content = f"""from kedro.config import OmegaConfigLoader
+CONFIG_LOADER_CLASS = OmegaConfigLoader
+CONFIG_LOADER_ARGS = {{
+    "config_patterns": {{
+        "airflow": [{patterns}],  # configure the pattern for configuration files
+    }}
+}}
+"""
+    file_name.write_text(content)
+
+
+@fixture(scope="session")
+def kedro_project(cli_runner):  # pylint: disable=unused-argument
+    tmp_path = Path().cwd()
+    # From `kedro-mlflow.tests.conftest.py`
+    config = {
+        "output_dir": tmp_path,
+        "kedro_version": kedro_version,
+        "project_name": "This is a fake project",
+        "repo_name": "fake-project",
+        "python_package": "fake_project",
+        "include_example": True,
+    }
+
+    cookiecutter(
+        str(TEMPLATE_PATH),
+        output_dir=config["output_dir"],
+        no_input=True,
+        extra_context=config,
+    )
+
+    pipeline_registry_py = """
+from kedro.pipeline import Pipeline, node
+
+
+def identity(arg):
+    return arg
+
+
+def register_pipelines():
+    pipeline = Pipeline(
+        [
+            node(identity, ["input"], ["intermediate"], name="node0"),
+            node(identity, ["intermediate"], ["output"], name="node1"),
+        ],
+        tags="pipeline0",
+    )
+    return {
+        "__default__": pipeline,
+        "ds": pipeline,
+    }
+    """
+
+    project_path = tmp_path / "fake-project"
+    (project_path / "src" / "fake_project" / "pipeline_registry.py").write_text(
+        pipeline_registry_py
+    )
+
+    settings_file = project_path / "src" / "fake_project" / "settings.py"
+    _create_kedro_settings_py(
+        settings_file, ["airflow*", "airflow/**", "scheduler*", "scheduler/**"]
+    )
+
+    os.chdir(project_path)
+    return project_path
+
+
+@fixture(scope="session")
+def metadata(kedro_project):  # pylint: disable=unused-argument
     # cwd() depends on ^ the isolated filesystem, created by CliRunner()
-    project_path = Path.cwd()
+    project_path = kedro_project
     return ProjectMetadata(
         project_path / "pyproject.toml",
         "hello_world",

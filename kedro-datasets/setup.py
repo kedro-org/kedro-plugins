bfe93f9ef20c6b7c15f87ecf57e76946ea5a4f5e
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-08-23 16:59:37 +0100

    update pandas pin

--- kedro-datasets/setup.py
+++ kedro-datasets/setup.py
@@ -3,7 +3,7 @@ from itertools import chain
 from setuptools import setup
 
 # at least 1.3 to be able to use XMLDataSet and pandas integration with fsspec
-PANDAS = "pandas~=1.3"
+PANDAS = "pandas>=1.3, <3.0"
 SPARK = "pyspark>=2.2, <3.4"
 HDFS = "hdfs>=2.5.8, <3.0"
 S3FS = "s3fs>=0.3.0, <0.5"

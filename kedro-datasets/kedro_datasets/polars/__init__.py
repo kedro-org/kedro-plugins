3161003add74d3a6e4a99bd3bda24db04f22de1f
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-08-26 11:52:14 +0200

    Fix
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- kedro-datasets/kedro_datasets/polars/__init__.py
+++ kedro-datasets/kedro_datasets/polars/__init__.py
@@ -5,6 +5,7 @@ import lazy_loader as lazy
 
 # https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
 CSVDataSet: Any
+GenericDataSet: Any
 
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,

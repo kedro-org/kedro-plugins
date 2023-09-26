252afe472761aa414a8620956460927059831b8d
Felix Scherz <felixwscherz@gmail.com>
2023-09-26 23:24:06 +0200

    feat(datasets): make `GeoJSONDataset.__init__` keyword only
    Signed-off-by: Felix Scherz <felixwscherz@gmail.com>

--- kedro-datasets/kedro_datasets/geopandas/geojson_dataset.py
+++ kedro-datasets/kedro_datasets/geopandas/geojson_dataset.py
@@ -50,6 +50,7 @@ class GeoJSONDataset(
 
     def __init__(  # noqa: PLR0913
         self,
+        *,
         filepath: str,
         load_args: Dict[str, Any] = None,
         save_args: Dict[str, Any] = None,

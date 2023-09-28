d5b4ecdc48d6275d5e7d3ae7f0410bae6ce94509
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-09-28 07:50:39 -0600

    feat(datasets): create custom `DeprecationWarning`
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- kedro-datasets/kedro_datasets/__init__.py
+++ kedro-datasets/kedro_datasets/__init__.py
@@ -1,3 +1,12 @@
 """``kedro_datasets`` is where you can find all of Kedro's data connectors."""
 
+__all__ = ["KedroDeprecationWarning"]
 __version__ = "1.7.0"
+
+try:
+    # Custom `KedroDeprecationWarning` class was added in Kedro 0.18.14.
+    from kedro import KedroDeprecationWarning
+except ImportError:
+
+    class KedroDeprecationWarning(DeprecationWarning):
+        """Custom class for warnings about deprecated Kedro features."""

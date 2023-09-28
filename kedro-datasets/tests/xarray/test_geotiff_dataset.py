1fa31c7043dcb4326829352e8c7628a9be5abf71
tgoelles <thomas.goelles@gmail.com>
2023-09-28 07:46:00 +0200

    reformat with black
    Signed-off-by: tgoelles <thomas.goelles@gmail.com>

--- kedro-datasets/tests/xarray/test_geotiff_dataset.py
+++ kedro-datasets/tests/xarray/test_geotiff_dataset.py
@@ -10,20 +10,22 @@ from kedro_datasets.xarray import GeoTiffDataset
 def cog_file_path():
     return Path(__file__).parent / "cog.tif"
 
+
 @pytest.fixture
 def geotiff_dataset(cog_file_path, save_args, fs_args) -> GeoTiffDataset:
     return GeoTiffDataset(filepath=cog_file_path, save_args=save_args, fs_args=fs_args)
 
+
 @pytest.fixture
-def versioned_geotiff_dataset(filepath_yaml, load_version, save_version) -> GeoTiffDataset:
+def versioned_geotiff_dataset(
+    filepath_yaml, load_version, save_version
+) -> GeoTiffDataset:
     return GeoTiffDataset(
         filepath=filepath_yaml, version=Version(load_version, save_version)
     )
-
 
 
 def test_save_and_load(self, geotiff_dataset, cog_file_path):
     """Test saving and reloading the data set."""
     loaded_tiff = geotiff_dataset.load()
     assert type(loaded_tiff) == rioxarray.raster_dataset.RasterArray
-

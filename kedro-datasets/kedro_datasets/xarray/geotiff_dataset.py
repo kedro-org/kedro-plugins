c6d54eacb93ed55156b68b8f8e20cb3727227f10
tgoelles <thomas.goelles@gmail.com>
2023-09-28 11:15:51 +0200

    class docstring
    Signed-off-by: tgoelles <thomas.goelles@gmail.com>

--- kedro-datasets/kedro_datasets/xarray/geotiff_dataset.py
+++ kedro-datasets/kedro_datasets/xarray/geotiff_dataset.py
@@ -16,6 +16,8 @@ from kedro_datasets._io import AbstractVersionedDataset, DatasetError
 
 
 class GeoTiffDataset(AbstractVersionedDataset[xarray.DataArray, xarray.DataArray]):
+    """``GeoTiffDataset``  loads and saves geotiff files and reads them as xarray
+    DataArrays. """
     def __init__(
         self,
         filepath: str,

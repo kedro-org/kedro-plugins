b51adc6199cf638ff10b731888cac432b23ffce9
Felix Scherz <felixwscherz@gmail.com>
2023-09-28 19:13:24 +0200

    feat(datasets): make `SparkHiveDataset.__init__` keyword only
    Signed-off-by: Felix Scherz <felixwscherz@gmail.com>

--- kedro-datasets/kedro_datasets/spark/spark_hive_dataset.py
+++ kedro-datasets/kedro_datasets/spark/spark_hive_dataset.py
@@ -72,6 +72,7 @@ class SparkHiveDataset(AbstractDataset[DataFrame, DataFrame]):
 
     def __init__(  # noqa: PLR0913
         self,
+        *,
         database: str,
         table: str,
         write_mode: str = "errorifexists",

a506fd70151d771a82f4b5c979caad9f3ecde7dd
Felix Scherz <felixwscherz@gmail.com>
2023-09-28 18:47:47 +0200

    feat(datasets): make `FeatherDataset.__init__` keyword only
    Signed-off-by: Felix Scherz <felixwscherz@gmail.com>

--- kedro-datasets/kedro_datasets/pandas/feather_dataset.py
+++ kedro-datasets/kedro_datasets/pandas/feather_dataset.py
@@ -73,6 +73,7 @@ class FeatherDataset(AbstractVersionedDataset[pd.DataFrame, pd.DataFrame]):
 
     def __init__(  # noqa: PLR0913
         self,
+        *,
         filepath: str,
         load_args: Dict[str, Any] = None,
         save_args: Dict[str, Any] = None,

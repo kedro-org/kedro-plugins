3e3569b67bfcf5225d44b7385c9ff153a61e68ca
tgoelles <thomas.goelles@gmail.com>
2023-10-05 13:14:36 +0200

    basic test suite
    Signed-off-by: tgoelles <thomas.goelles@gmail.com>

--- kedro-datasets/tests/xarray/test_geotiff_dataset.py
+++ kedro-datasets/tests/xarray/test_geotiff_dataset.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 import pytest
-import xarray as xr
 import rioxarray
+import xarray as xr
 from kedro.io.core import Version
 
+from kedro_datasets._io import DatasetError
 from kedro_datasets.xarray import GeoTiffDataset
 
 
@@ -13,45 +14,42 @@ def cog_file_path() -> str:
     cog_file_path = Path(__file__).parent / "cog.tif"
     return cog_file_path.as_posix()
 
+
 @pytest.fixture
 def cog_xarray(cog_file_path) -> xr.DataArray:
     return rioxarray.open_rasterio(cog_file_path)
 
+
 @pytest.fixture
-def cog_geotiff_dataset(cog_file_path, save_args, fs_args) -> GeoTiffDataset:
+def cog_geotiff_dataset(cog_file_path, save_args) -> GeoTiffDataset:
     return GeoTiffDataset(filepath=cog_file_path, save_args=save_args)
+
 
 @pytest.fixture
 def filepath_geotiff(tmp_path):
     return (tmp_path / "test.tiff").as_posix()
+
 
 @pytest.fixture
 def geotiff_dataset(filepath_geotiff, save_args):
     return GeoTiffDataset(filepath=filepath_geotiff, save_args=save_args)
 
 
-@pytest.fixture
-def versioned_geotiff_dataset(
-    filepath_yaml, load_version, save_version
-) -> GeoTiffDataset:
-    return GeoTiffDataset(
-        filepath=filepath_yaml, version=Version(load_version, save_version)
-    )
-
-
 def test_load(cog_geotiff_dataset):
     """Test saving and reloading the data set."""
     loaded_tiff = cog_geotiff_dataset.load()
     assert isinstance(loaded_tiff, xr.DataArray)
-    assert loaded_tiff.shape == (1,500,500)
-    assert loaded_tiff.dims == ('band', 'y', 'x')
+    assert loaded_tiff.shape == (1, 500, 500)
+    assert loaded_tiff.dims == ("band", "y", "x")
+
 
 def test_exists(geotiff_dataset, cog_xarray):
-        """Test `exists` method invocation for both existing and
-        nonexistent data set."""
-        assert not geotiff_dataset.exists()
-        geotiff_dataset.save(cog_xarray)
-        assert geotiff_dataset.exists()
+    """Test `exists` method invocation for both existing and
+    nonexistent data set."""
+    assert not geotiff_dataset.exists()
+    geotiff_dataset.save(cog_xarray)
+    assert geotiff_dataset.exists()
+
 
 def test_save_and_load(geotiff_dataset, cog_xarray):
     """Test saving and reloading the data set."""

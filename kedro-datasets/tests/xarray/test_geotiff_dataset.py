dc47f7642eba9d3b6269ee223a722ab06719f1cc
tgoelles <thomas.goelles@gmail.com>
2023-10-05 10:51:32 +0200

    test reloaded
    Signed-off-by: tgoelles <thomas.goelles@gmail.com>

--- kedro-datasets/tests/xarray/test_geotiff_dataset.py
+++ kedro-datasets/tests/xarray/test_geotiff_dataset.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 import xarray as xr
+import rioxarray
 from kedro.io.core import Version
 
 from kedro_datasets.xarray import GeoTiffDataset
@@ -12,10 +13,21 @@ def cog_file_path() -> str:
     cog_file_path = Path(__file__).parent / "cog.tif"
     return cog_file_path.as_posix()
 
+@pytest.fixture
+def cog_xarray(cog_file_path) -> xr.DataArray:
+    return rioxarray.open_rasterio(cog_file_path)
 
 @pytest.fixture
-def geotiff_dataset(cog_file_path, save_args, fs_args) -> GeoTiffDataset:
+def cog_geotiff_dataset(cog_file_path, save_args, fs_args) -> GeoTiffDataset:
     return GeoTiffDataset(filepath=cog_file_path, save_args=save_args)
+
+@pytest.fixture
+def filepath_geotiff(tmp_path):
+    return (tmp_path / "test.tiff").as_posix()
+
+@pytest.fixture
+def geotiff_dataset(filepath_geotiff, save_args):
+    return GeoTiffDataset(filepath=filepath_geotiff, save_args=save_args)
 
 
 @pytest.fixture
@@ -27,7 +39,18 @@ def versioned_geotiff_dataset(
     )
 
 
-def test_save_and_load(geotiff_dataset):
+def test_load(cog_geotiff_dataset):
     """Test saving and reloading the data set."""
-    loaded_tiff = geotiff_dataset.load()
+    loaded_tiff = cog_geotiff_dataset.load()
     assert isinstance(loaded_tiff, xr.DataArray)
+    assert loaded_tiff.shape == (1,500,500)
+    assert loaded_tiff.dims == ('band', 'y', 'x')
+
+
+def test_save_and_load(geotiff_dataset, cog_xarray):
+    """Test saving and reloading the data set."""
+    geotiff_dataset.save(cog_xarray)
+    reloaded = geotiff_dataset.load()
+    assert reloaded.shape == cog_xarray.shape
+    assert reloaded.dims == cog_xarray.dimsts
+    assert reloaded.equals(cog_xarray)

869e19fd130fe9b48d0955ca1536f3ba528a7015
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2023-10-05 14:25:55 +0100

    Revert trying to fix airflow
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- kedro-airflow/features/steps/cli_steps.py
+++ kedro-airflow/features/steps/cli_steps.py
@@ -86,9 +86,9 @@ def prepare_catalog(context):
 def install_kedro(context, version):
     """Execute Kedro command and check the status."""
     if version == "latest":
-        cmd = [context.pip, "install", "-U", "kedro-datasets[CSVDataSet]"]
+        cmd = [context.pip, "install", "-U", "kedro-datasets[PANDAS]"]
     else:
-        cmd = [context.pip, "install", f"kedro-datasets[CSVDataSet]=={version}"]
+        cmd = [context.pip, "install", f"kedro-datasets[PANDAS]=={version}"]
     res = run(cmd, env=context.env)
 
     if res.returncode != OK_EXIT_CODE:

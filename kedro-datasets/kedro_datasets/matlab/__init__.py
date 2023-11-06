dba9dc813a85c10181909d2ddcaa0d3f5a5f4420
samuelleeshemen <samuel_lee_sj@aiap.sg>
2023-11-06 14:19:43 +0800

    matlab_dataset init
    Signed-off-by: samuelleeshemen <samuel_lee_sj@aiap.sg>

--- kedro-datasets/kedro_datasets/matlab/__init__.py
+++ kedro-datasets/kedro_datasets/matlab/__init__.py
@@ -1,13 +1,14 @@
-"""``AbstractDataset`` implementation to load/save data from/to a Matlab file."""
+"""``AbstractDataset`` implementation to load/save data from/to a Pickle file."""
 from __future__ import annotations
 
 from typing import Any
 
 import lazy_loader as lazy
 
-MatlabDataSet: type[MatlabDataset]
-MatlabDataset: Any
+# https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
+PickleDataSet: type[PickleDataset]
+PickleDataset: Any
 
 __getattr__, __dir__, __all__ = lazy.attach(
-    __name__, submod_attrs={"matlab_dataset": ["MatlabDataSet", "MatlabDataset"]}
+    __name__, submod_attrs={"pickle_dataset": ["PickleDataSet", "PickleDataset"]}
 )

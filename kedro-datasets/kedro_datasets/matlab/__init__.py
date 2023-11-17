7ba05a81c598d1450ba102629e3b8402109463d1
samuelleeshemen <samuel_lee_sj@aiap.sg>
2023-11-17 10:10:43 +0800

    Completed pytest on the matlab dataset
    Signed-off-by: samuelleeshemen <samuel_lee_sj@aiap.sg>

--- kedro-datasets/kedro_datasets/matlab/__init__.py
+++ kedro-datasets/kedro_datasets/matlab/__init__.py
@@ -6,9 +6,9 @@ from typing import Any
 import lazy_loader as lazy
 
 # https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
-PickleDataSet: type[PickleDataset]
-PickleDataset: Any
+MatlabDataSet: type[MatlabDataset]
+MatlabDataset: Any
 
 __getattr__, __dir__, __all__ = lazy.attach(
-    __name__, submod_attrs={"pickle_dataset": ["PickleDataSet", "PickleDataset"]}
+    __name__, submod_attrs={"matlab_dataset": ["MatlabDataSet", "MatlabDataset"]}
 )

afe6e7e6bb6fcc630bafc1343485e1c507de002b
samuelleeshemen <samuel_lee_sj@aiap.sg>
2023-11-20 09:36:04 +0800

    Reviewed and resolved errors in code 201123
    Signed-off-by: samuelleeshemen <samuel_lee_sj@aiap.sg>

--- kedro-datasets/kedro_datasets/matlab/__init__.py
+++ kedro-datasets/kedro_datasets/matlab/__init__.py
@@ -1,4 +1,4 @@
-"""``AbstractDataset`` implementation to load/save data from/to a Pickle file."""
+"""``AbstractDataset`` implementation to load/save data from/to a Matlab file."""
 from __future__ import annotations
 
 from typing import Any

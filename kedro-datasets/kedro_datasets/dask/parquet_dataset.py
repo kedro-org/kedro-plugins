2cf459a67ddc30535a8d93595616c3324d78baf2
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-11-20 12:56:47 -0600

    test(datasets): fix `dask.ParquetDataset` doctests
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- kedro-datasets/kedro_datasets/dask/parquet_dataset.py
+++ kedro-datasets/kedro_datasets/dask/parquet_dataset.py
@@ -42,25 +42,33 @@ class ParquetDataset(AbstractDataset[dd.DataFrame, dd.DataFrame]):
 
         >>> import dask.dataframe as dd
         >>> import pandas as pd
+        >>> import pyarrow as pa
         >>> from kedro_datasets.dask import ParquetDataset
+        >>> from pandas.testing import assert_frame_equal
         >>>
         >>> data = pd.DataFrame({"col1": [1, 2], "col2": [4, 5], "col3": [[5, 6], [7, 8]]})
         >>> ddf = dd.from_pandas(data, npartitions=2)
         >>>
+        >>> # Define schema using PyArrow data types
+        >>> schema = pa.schema(
+        ...     [
+        ...         ("col1", pa.int64()),  # Integer column
+        ...         ("col2", pa.int64()),  # Integer column
+        ...         ("col3", pa.list_(pa.int64())),  # List of integers
+        ...     ]
+        ... )
+        >>>
         >>> dataset = ParquetDataset(
-        ...     filepath="s3://bucket_name/path/to/folder",
-        ...     credentials={
-        ...         "client_kwargs": {
-        ...             "aws_access_key_id": "YOUR_KEY",
-        ...             "aws_secret_access_key": "YOUR SECRET",
-        ...         }
+        ...     filepath="path/to/folder",
+        ...     save_args={
+        ...         "compression": "GZIP",
+        ...         "schema": schema,
         ...     },
-        ...     save_args={"compression": "GZIP"},
         ... )
         >>> dataset.save(ddf)
         >>> reloaded = dataset.load()
         >>>
-        >>> assert ddf.compute().equals(reloaded.compute())
+        >>> assert_frame_equal(ddf.compute(), reloaded.compute())
 
     The output schema can also be explicitly specified using
     `Triad <https://triad.readthedocs.io/en/latest/api/\

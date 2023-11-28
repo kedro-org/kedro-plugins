da0626520ec6d43fb093d6b1eb073dd416fd5a2d
Merel Theisen <merel.theisen@quantumblack.com>
2023-11-28 16:19:19 +0000

    Fix import
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- kedro-datasets/kedro_datasets/spark/spark_dataset.py
+++ kedro-datasets/kedro_datasets/spark/spark_dataset.py
@@ -233,7 +233,7 @@ class SparkDataset(AbstractVersionedDataset[DataFrame, DataFrame]):
     .. code-block:: pycon
 
         >>> from pyspark.sql import SparkSession
-        >>> from pyspark.sql.types import IntegerType, Row, StructField, StructType
+        >>> from pyspark.sql.types import IntegerType, Row, StringType, StructField, StructType
         >>>
         >>> from kedro_datasets.spark import SparkDataset
         >>>

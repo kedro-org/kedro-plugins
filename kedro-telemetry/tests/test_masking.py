bd59842d351862bae42b1eea4e1f85188236c649
Sajid Alam <sajid_alam@mckinsey.com>
2023-12-12 15:48:58 +0000

    remove outdated commands

--- kedro-telemetry/tests/test_masking.py
+++ kedro-telemetry/tests/test_masking.py
@@ -19,13 +19,9 @@ from kedro_telemetry.masking import (
 REPO_NAME = "cli_tools_dummy_project"
 PACKAGE_NAME = "cli_tools_dummy_package"
 DEFAULT_KEDRO_COMMANDS = [
-    "activate-nbstripout",
-    "build-docs",
-    "build-reqs",
     "catalog",
     "ipython",
     "jupyter",
-    "lint",
     "micropkg",
     "new",
     "package",
@@ -33,7 +29,6 @@ DEFAULT_KEDRO_COMMANDS = [
     "registry",
     "run",
     "starter",
-    "test",
 ]
 
 

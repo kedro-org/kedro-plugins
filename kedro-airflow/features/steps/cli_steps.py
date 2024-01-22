5d72353345a855379f1a17f14043275ef83ffef7
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2024-01-22 15:46:50 +0000

    Update create connections step
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- kedro-airflow/features/steps/cli_steps.py
+++ kedro-airflow/features/steps/cli_steps.py
@@ -12,9 +12,7 @@ OK_EXIT_CODE = 0
 def init_airflow(context, home_dir):
     context.airflow_dir = context.temp_dir / home_dir
     context.env["AIRFLOW_HOME"] = str(context.airflow_dir)
-    res = run(
-        [context.airflow, "connections", "create-default-connections"], env=context.env
-    )
+    res = run(["airflow", "connections", "create-default-connections"], env=context.env)
     assert res.returncode == 0
 
 

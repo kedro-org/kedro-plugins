652af3d9f73988916be57a8afa538a78fe77ae8d
rashidakanchwala <rashida_kanchwala@mckinsey.com>
2024-01-31 17:14:16 +0000

    remove comma
    Signed-off-by: rashidakanchwala <rashida_kanchwala@mckinsey.com>

--- kedro-datasets/docs/source/conf.py
+++ kedro-datasets/docs/source/conf.py
@@ -104,7 +104,7 @@ type_targets = {
         "requests.auth.AuthBase",
         "google.oauth2.credentials.Credentials",
         "deltalake.table.Metadata",
-        "DataCatalog",
+        "DataCatalog"
     ),
     "py:data": (
         "typing.Any",

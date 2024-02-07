51cf1de114a30e5d601fbfc669bdbfcab2828e4c
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2024-02-07 11:49:25 +0100

    Update kedro-datasets/setup.py
    Signed-off-by: Juan Luis Cano Rodríguez <hello@juanlu.space>

--- kedro-datasets/setup.py
+++ kedro-datasets/setup.py
@@ -184,7 +184,7 @@ extras_require["test"] = [
     "coverage[toml]",
     "dask[complete]>=2021.10",
     "delta-spark>=1.0, <3.0",
-    "deltalake>=0.10.0, <0.15.2",
+    "deltalake>=0.10.0, !=0.15.2",
     "dill~=0.3.1",
     "filelock>=3.4.0, <4.0",
     "gcsfs>=2023.1, <2023.3",

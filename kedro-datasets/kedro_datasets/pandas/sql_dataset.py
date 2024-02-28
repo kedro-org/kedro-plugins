dc0d2c78105ae4ebbb5bed1e9f851468ebc7c6cb
Manuel Spierenburg <manuel.spierenburg@gmail.com>
2024-02-28 10:58:26 +0100

    feat(datasets): allow additional parameters for sqlalchemy engine when using sql dataset (#564)
    * allow additional sqlalchemy engine parameters
    
    Signed-off-by: Manuel Spierenburg <manuel.spierenburg@gmail.com>

--- kedro-datasets/kedro_datasets/pandas/sql_dataset.py
+++ kedro-datasets/kedro_datasets/pandas/sql_dataset.py
@@ -126,6 +126,7 @@ class SQLTableDataset(AbstractDataset[pd.DataFrame, pd.DataFrame]):
 
         db_credentials:
           con: postgresql://scott:tiger@localhost/test
+          pool_size: 10 # additional parameters
 
     Example usage for the
     `Python API <https://kedro.readthedocs.io/en/stable/data/\
@@ -174,6 +175,8 @@ class SQLTableDataset(AbstractDataset[pd.DataFrame, pd.DataFrame]):
                 through credentials.
                 To find all supported connection string formats, see here:
                 https://docs.sqlalchemy.org/core/engines.html#database-urls
+                Additional parameters for the sqlalchemy engine can be provided
+                alongside the 'con' parameter.
             load_args: Provided to underlying pandas ``read_sql_table``
                 function along with the connection string.
                 To find all supported arguments, see here:
@@ -215,17 +218,20 @@ class SQLTableDataset(AbstractDataset[pd.DataFrame, pd.DataFrame]):
         self._save_args["name"] = table_name
 
         self._connection_str = credentials["con"]
+        self._connection_args = {
+            k: credentials[k] for k in credentials.keys() if k != "con"
+        }
 
         self.metadata = metadata
 
     @classmethod
-    def create_connection(cls, connection_str: str) -> None:
+    def create_connection(cls, connection_str: str, connection_args: dict) -> None:
         """Given a connection string, create singleton connection
         to be used across all instances of ``SQLTableDataset`` that
         need to connect to the same source.
         """
         try:
-            engine = create_engine(connection_str)
+            engine = create_engine(connection_str, **connection_args)
         except ImportError as import_error:
             raise _get_missing_module_error(import_error) from import_error
         except NoSuchModuleError as exc:
@@ -239,7 +245,7 @@ class SQLTableDataset(AbstractDataset[pd.DataFrame, pd.DataFrame]):
         cls = type(self)
 
         if self._connection_str not in cls.engines:
-            self.create_connection(self._connection_str)
+            self.create_connection(self._connection_str, self._connection_args)
 
         return cls.engines[self._connection_str]
 
@@ -309,6 +315,7 @@ class SQLQueryDataset(AbstractDataset[None, pd.DataFrame]):
 
         db_credentials:
           con: postgresql://scott:tiger@localhost/test
+          pool_size: 10 # additional parameters
 
     Example usage for the
     `Python API <https://kedro.readthedocs.io/en/stable/data/\
@@ -412,6 +419,8 @@ class SQLQueryDataset(AbstractDataset[None, pd.DataFrame]):
                 ``load_args`` and ``save_args`` in case it is provided. To find
                 all supported connection string formats, see here:
                 https://docs.sqlalchemy.org/core/engines.html#database-urls
+                Additional parameters for the sqlalchemy engine can be provided
+                alongside the 'con' parameter.
             load_args: Provided to underlying pandas ``read_sql_query``
                 function along with the connection string.
                 To find all supported arguments, see here:
@@ -480,18 +489,21 @@ class SQLQueryDataset(AbstractDataset[None, pd.DataFrame]):
             self._fs = fsspec.filesystem(self._protocol, **_fs_credentials, **_fs_args)
             self._filepath = path
         self._connection_str = credentials["con"]
+        self._connection_args = {
+            k: credentials[k] for k in credentials.keys() if k != "con"
+        }
         self._execution_options = execution_options or {}
         if "mssql" in self._connection_str:
             self.adapt_mssql_date_params()
 
     @classmethod
-    def create_connection(cls, connection_str: str) -> None:
+    def create_connection(cls, connection_str: str, connection_args: dict) -> None:
         """Given a connection string, create singleton connection
         to be used across all instances of `SQLQueryDataset` that
         need to connect to the same source.
         """
         try:
-            engine = create_engine(connection_str)
+            engine = create_engine(connection_str, **connection_args)
         except ImportError as import_error:
             raise _get_missing_module_error(import_error) from import_error
         except NoSuchModuleError as exc:
@@ -505,7 +517,7 @@ class SQLQueryDataset(AbstractDataset[None, pd.DataFrame]):
         cls = type(self)
 
         if self._connection_str not in cls.engines:
-            self.create_connection(self._connection_str)
+            self.create_connection(self._connection_str, self._connection_args)
 
         return cls.engines[self._connection_str]
 

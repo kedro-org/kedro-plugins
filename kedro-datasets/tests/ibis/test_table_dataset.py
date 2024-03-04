c29e9e1246e73d29df773ccb9609d6d351fbb592
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-03-04 10:04:44 -0700

    test(datasets): test extra load and save args work
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- kedro-datasets/tests/ibis/test_table_dataset.py
+++ kedro-datasets/tests/ibis/test_table_dataset.py
@@ -18,8 +18,13 @@ def connection_config(tmp_path):
 
 
 @pytest.fixture
-def table_dataset(connection_config):
-    return TableDataset(table_name="penguins", connection=connection_config)
+def table_dataset(connection_config, load_args, save_args):
+    return TableDataset(
+        table_name="penguins",
+        connection=connection_config,
+        load_args=load_args,
+        save_args=save_args,
+    )
 
 
 @pytest.fixture
@@ -42,3 +47,19 @@ class TestTableDataset:
         assert not table_dataset.exists()
         table_dataset.save(dummy_table)
         assert table_dataset.exists()
+
+    @pytest.mark.parametrize(
+        "load_args", [{"k1": "v1", "index": "value"}], indirect=True
+    )
+    def test_load_extra_params(self, table_dataset, load_args):
+        """Test overriding the default load arguments."""
+        for key, value in load_args.items():
+            assert table_dataset._load_args[key] == value
+
+    @pytest.mark.parametrize(
+        "save_args", [{"k1": "v1", "index": "value"}], indirect=True
+    )
+    def test_save_extra_params(self, table_dataset, save_args):
+        """Test overriding the default save arguments."""
+        for key, value in save_args.items():
+            assert table_dataset._save_args[key] == value

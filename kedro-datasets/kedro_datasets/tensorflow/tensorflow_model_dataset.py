894ac24983f80007cc3fd37cd2d054a9c3273630
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2024-03-14 08:12:41 +0100

    Whitespace fix
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- kedro-datasets/kedro_datasets/tensorflow/tensorflow_model_dataset.py
+++ kedro-datasets/kedro_datasets/tensorflow/tensorflow_model_dataset.py
@@ -53,7 +53,7 @@ class TensorFlowModelDataset(AbstractVersionedDataset[tf.keras.Model, tf.keras.M
         >>> dataset = TensorFlowModelDataset(filepath=tmp_path / "data/06_models/tensorflow_model.h5")
         >>> model = tf.keras.Sequential([tf.keras.layers.Dense(5, input_shape=(3,)),tf.keras.layers.Softmax()])
         >>>
-        >>> # x = tf.random.uniform((10, 3))
+        >>> # x = tf.random.uniform((10, 3))
         >>> # predictions = model.predict(x)
         >>>
         >>> dataset.save(model)

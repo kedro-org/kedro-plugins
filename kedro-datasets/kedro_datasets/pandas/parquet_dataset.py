921bdb8b12463d8d65b09856598dd52c28f5a2f0
Ahdra Merali <ahdra.merali@quantumblack.com>
2024-04-11 18:53:27 +0100

    First pass pandas/ParquetDataset
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>

--- kedro-datasets/kedro_datasets/pandas/parquet_dataset.py
+++ kedro-datasets/kedro_datasets/pandas/parquet_dataset.py
@@ -81,12 +81,12 @@ class ParquetDataset(AbstractVersionedDataset[pd.DataFrame, pd.DataFrame]):
         self,
         *,
         filepath: str,
-        load_args: dict[str, Any] = None,
-        save_args: dict[str, Any] = None,
-        version: Version = None,
-        credentials: dict[str, Any] = None,
-        fs_args: dict[str, Any] = None,
-        metadata: dict[str, Any] = None,
+        load_args: dict[str, Any] | None = None,
+        save_args: dict[str, Any] | None = None,
+        version: Version | None = None,
+        credentials: dict[str, Any] | None = None,
+        fs_args: dict[str, Any] | None = None,
+        metadata: dict[str, Any] | None = None,
     ) -> None:
         """Creates a new instance of ``ParquetDataset`` pointing to a concrete Parquet file
         on a specific filesystem.

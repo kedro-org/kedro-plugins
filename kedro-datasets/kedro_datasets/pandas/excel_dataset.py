37bd7793dfa6c3e9853deb0c8653edc90ceaa5b2
Ahdra Merali <ahdra.merali@quantumblack.com>
2024-04-11 18:46:15 +0100

    First pass pandas/ExcelDataset
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>

--- kedro-datasets/kedro_datasets/pandas/excel_dataset.py
+++ kedro-datasets/kedro_datasets/pandas/excel_dataset.py
@@ -116,12 +116,12 @@ class ExcelDataset(
         *,
         filepath: str,
         engine: str = "openpyxl",
-        load_args: dict[str, Any] = None,
-        save_args: dict[str, Any] = None,
-        version: Version = None,
-        credentials: dict[str, Any] = None,
-        fs_args: dict[str, Any] = None,
-        metadata: dict[str, Any] = None,
+        load_args: dict[str, Any] | None = None,
+        save_args: dict[str, Any] | None = None,
+        version: Version | None = None,
+        credentials: dict[str, Any] | None = None,
+        fs_args: dict[str, Any] | None = None,
+        metadata: dict[str, Any] | None = None,
     ) -> None:
         """Creates a new instance of ``ExcelDataset`` pointing to a concrete Excel file
         on a specific filesystem.

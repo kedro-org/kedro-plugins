e342e960c87c61bfd327ea6579fea941c3cf1e80
Elena Khaustova <ymax70rus@gmail.com>
2024-05-03 21:29:07 +0100

    Fixing docs build
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro-datasets/docs/source/conf.py
+++ kedro-datasets/docs/source/conf.py
@@ -106,7 +106,7 @@ type_targets = {
         "google.oauth2.credentials.Credentials",
         "deltalake.table.Metadata",
         "DataCatalog",
-        "ibis.backends.base.BaseBackend",
+        "ibis.base.BaseBackend",
     ),
     "py:data": (
         "typing.Any",

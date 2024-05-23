f67c88ec315c292b59bf5a7a1d75e2e55b5e6ed1
Sajid Alam <sajid_alam@mckinsey.com>
2024-05-23 11:44:39 +0100

    Update managed_table_dataset.py
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- kedro-datasets/kedro_datasets/databricks/managed_table_dataset.py
+++ kedro-datasets/kedro_datasets/databricks/managed_table_dataset.py
@@ -394,12 +394,13 @@ class ManagedTableDataset(AbstractVersionedDataset):
             cols = schema.fieldNames()
             if self._table.dataframe_type == "pandas":
                 data = _get_spark().createDataFrame(
-                    data.loc[:, cols].to_dict(orient='list'), schema=schema
+                    data[cols].to_dict(orient='records'), schema=schema
                 )
             else:
                 data = data.select(*cols)
         elif self._table.dataframe_type == "pandas":
-            data = _get_spark().createDataFrame(data.to_dict(orient='list'))
+            data = _get_spark().createDataFrame(data.to_dict(orient='records'))
+
         if self._table.write_mode == "overwrite":
             self._save_overwrite(data)
         elif self._table.write_mode == "upsert":

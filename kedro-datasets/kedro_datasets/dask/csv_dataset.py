666bf722a6f19127c549fd78a8632136c84be7c0
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2024-06-11 15:58:57 +0100

    Fix lint and test
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- kedro-datasets/kedro_datasets/dask/csv_dataset.py
+++ kedro-datasets/kedro_datasets/dask/csv_dataset.py
@@ -1,5 +1,6 @@
 """``CSVDataset`` is a data set used to load and save data to CSV files using Dask
 dataframe"""
+from __future__ import annotations
 
 from copy import deepcopy
 from typing import Any
@@ -7,10 +8,6 @@ from typing import Any
 import dask.dataframe as dd
 import fsspec
 from kedro.io.core import AbstractDataset, get_protocol_and_path
-
-# NOTE: kedro.extras.datasets will be removed in Kedro 0.19.0.
-# Any contribution to datasets should be made in kedro-datasets
-# in kedro-plugins (https://github.com/kedro-org/kedro-plugins)
 
 
 class CSVDataset(AbstractDataset[dd.DataFrame, dd.DataFrame]):
@@ -37,26 +34,24 @@ class CSVDataset(AbstractDataset[dd.DataFrame, dd.DataFrame]):
     Example usage for the
     `Python API <https://kedro.readthedocs.io/en/stable/data/\
     advanced_data_catalog_usage.html>`_:
-    ::
 
+    .. code-block:: pycon
 
         >>> from kedro_datasets.dask import CSVDataset
         >>> import pandas as pd
         >>> import dask.dataframe as dd
         >>>
-        >>> data = pd.DataFrame({'col1': [1, 2], 'col2': [4, 5],  'col3': [[5, 6], [7, 8]]})
+        >>> data = pd.DataFrame({"col1": [1, 2], "col2": [4, 5], "col3": [[5, 6], [7, 8]]})
         >>> ddf = dd.from_pandas(data, npartitions=1)
         >>>
-        >>> data_set = CSVDataset(
-        ...     filepath="path/to/folder/*.csv"
-        ... )
+        >>> data_set = CSVDataset(filepath=tmppath / "path/to/folder/*.csv")
         >>> data_set.save(ddf)
         >>> reloaded = data_set.load()
         >>>
         >>> assert ddf.compute().equals(reloaded.compute())
     """
 
-    DEFAULT_LOAD_ARGS = {}  # type: Dict[str, Any]
+    DEFAULT_LOAD_ARGS: dict[str, Any] = {}
     DEFAULT_SAVE_ARGS: dict[str, Any] = {"index": False}
 
     def __init__(  # noqa: PLR0913
@@ -66,6 +61,7 @@ class CSVDataset(AbstractDataset[dd.DataFrame, dd.DataFrame]):
         save_args: dict[str, Any] = None,
         credentials: dict[str, Any] = None,
         fs_args: dict[str, Any] = None,
+        metadata: dict[str, Any] | None = None,
     ) -> None:
         """Creates a new instance of ``CSVDataset`` pointing to concrete
         CSV files.
@@ -81,10 +77,14 @@ class CSVDataset(AbstractDataset[dd.DataFrame, dd.DataFrame]):
                 E.g. for ``GCSFileSystem`` it should look like `{"token": None}`.
             fs_args: Optional parameters to the backend file system driver:
                 https://docs.dask.org/en/latest/how-to/connect-to-remote-data.html#optional-parameters
+            metadata: Any arbitrary metadata.
+                This is ignored by Kedro, but may be consumed by users or external plugins.
         """
         self._filepath = filepath
         self._fs_args = deepcopy(fs_args) or {}
         self._credentials = deepcopy(credentials) or {}
+
+        self.metadata = metadata
 
         # Handle default load and save arguments
         self._load_args = deepcopy(self.DEFAULT_LOAD_ARGS)

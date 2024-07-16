ff7564d9c391d6194a5445450a10fbe50e2fd2ea
Dmitry Sorokin <dmd40in@gmail.com>
2024-07-16 15:43:20 +0100

    revert Hive support
    Signed-off-by: Dmitry Sorokin <dmd40in@gmail.com>

--- kedro-datasets/tests/databricks/conftest.py
+++ kedro-datasets/tests/databricks/conftest.py
@@ -23,7 +23,6 @@ def spark_session():
             "spark.sql.catalog.spark_catalog",
             "org.apache.spark.sql.delta.catalog.DeltaCatalog",
         )
-        .enableHiveSupport()
         .getOrCreate()
     )
     spark.sql("create database if not exists test")

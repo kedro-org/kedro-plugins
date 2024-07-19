98401ed25a130cfea8716c0662b8706b173d5e44
Dmitry Sorokin <dmd40in@gmail.com>
2024-07-19 11:22:49 +0100

    Fix integration tests
    Signed-off-by: Dmitry Sorokin <dmd40in@gmail.com>

--- kedro-telemetry/tests/integration/test_telemetry.py
+++ kedro-telemetry/tests/integration/test_telemetry.py
@@ -5,12 +5,20 @@ from kedro.framework.cli.cli import KedroCLI
 from kedro.framework.session import KedroSession
 from kedro.framework.startup import bootstrap_project
 from pytest import fixture
+import shutil
+import tempfile
 from kedro_telemetry.plugin import KedroTelemetryHook
 
 
 @fixture
 def dummy_project_path():
-    return Path(__file__).parent / "dummy-project"
+    temp_dir = tempfile.mkdtemp()
+    template_project_path = Path(__file__).parent / "dummy-project"
+    shutil.copytree(template_project_path, Path(temp_dir), dirs_exist_ok=True)
+
+    yield Path(temp_dir)
+
+    shutil.rmtree(temp_dir)
 
 
 @fixture()
@@ -25,40 +33,37 @@ class TestKedroTelemetryHookIntegration:
         self, mocker, dummy_project_path, reset_telemetry
     ):
         telemetry_hook = reset_telemetry
-        mocked_heap_call = mocker.patch.object(telemetry_hook, "_send_heap_event")
-        mocked_consent_check_call = mocker.patch.object(
-            telemetry_hook, "_check_for_telemetry_consent", return_value=True
+        mocked_heap_call = mocker.patch.object(
+            telemetry_hook, "_send_telemetry_heap_event"
         )
+
         kedro_cli = KedroCLI(dummy_project_path)
         result = CliRunner().invoke(kedro_cli, ["run"])
         assert result.exit_code == 0
         mocked_heap_call.assert_called_once()
-        mocked_consent_check_call.assert_called_once()
 
     def test_telemetry_sent_once_with_other_kedro_command(
         self, mocker, dummy_project_path, reset_telemetry
     ):
         telemetry_hook = reset_telemetry
-        mocked_heap_call = mocker.patch.object(telemetry_hook, "_send_heap_event")
-        mocked_consent_check_call = mocker.patch.object(
-            telemetry_hook, "_check_for_telemetry_consent", return_value=True
+        mocked_heap_call = mocker.patch.object(
+            telemetry_hook, "_send_telemetry_heap_event"
         )
+
         kedro_cli = KedroCLI(dummy_project_path)
         result = CliRunner().invoke(kedro_cli, ["info"])
         assert result.exit_code == 0
         mocked_heap_call.assert_called_once()
-        mocked_consent_check_call.assert_called_once()
 
     def test_telemetry_sent_once_with_session_run(
         self, mocker, dummy_project_path, reset_telemetry
     ):
         telemetry_hook = reset_telemetry
-        mocked_heap_call = mocker.patch.object(telemetry_hook, "_send_heap_event")
-        mocked_consent_check_call = mocker.patch.object(
-            telemetry_hook, "_check_for_telemetry_consent", return_value=True
+        mocked_heap_call = mocker.patch.object(
+            telemetry_hook, "_send_telemetry_heap_event"
         )
+
         bootstrap_project(dummy_project_path)
         with KedroSession.create(project_path=dummy_project_path) as session:
             session.run()
         mocked_heap_call.assert_called_once()
-        mocked_consent_check_call.assert_called_once()

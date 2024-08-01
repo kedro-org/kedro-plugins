bd94dfa4f48123e7180228ff7a494072d26ed0a4
Elena Khaustova <ymax70rus@gmail.com>
2024-08-01 12:22:50 +0100

    Updates version
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro-datasets/kedro_datasets/__init__.py
+++ kedro-datasets/kedro_datasets/__init__.py
@@ -1,7 +1,7 @@
 """``kedro_datasets`` is where you can find all of Kedro's data connectors."""
 
 __all__ = ["KedroDeprecationWarning"]
-__version__ = "4.0.0"
+__version__ = "4.0.1"
 
 import sys
 import warnings

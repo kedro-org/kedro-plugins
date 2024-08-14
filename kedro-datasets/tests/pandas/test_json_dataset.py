0725a9646d4145a0b0b6322384a0a0866441ee1c
Merel Theisen <merel.theisen@quantumblack.com>
2024-08-14 17:27:22 +0100

    Add tests for coverage
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- kedro-datasets/tests/pandas/test_json_dataset.py
+++ kedro-datasets/tests/pandas/test_json_dataset.py
@@ -84,6 +84,16 @@ class TestJSONDataset:
             assert json_dataset._save_args[key] == value
 
     @pytest.mark.parametrize(
+        "fs_args",
+        [{"open_args_load": {"k1": "v1"}, "open_args_save": {"index": "value"}}],
+        indirect=True,
+    )
+    def test_fs_extra_params(self, json_dataset, fs_args):
+        """Test overriding the default fs arguments."""
+        assert json_dataset._fs_open_args_load == {"k1": "v1"}
+        assert json_dataset._fs_open_args_save == {"index": "value"}
+
+    @pytest.mark.parametrize(
         "load_args,save_args",
         [
             ({"storage_options": {"a": "b"}}, {}),

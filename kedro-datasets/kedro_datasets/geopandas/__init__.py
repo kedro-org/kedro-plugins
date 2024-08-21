c986369f4703fadd1a66cb3fb66eed20365d32dc
Harm Matthias Harms <matthias.harms@quis.de>
2024-08-21 16:33:12 +0200

    Fix pylint type
    Signed-off-by: Harm Matthias Harms <matthias.harms@quis.de>

--- kedro-datasets/kedro_datasets/geopandas/__init__.py
+++ kedro-datasets/kedro_datasets/geopandas/__init__.py
@@ -6,6 +6,7 @@ import lazy_loader as lazy
 
 # https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
 GeoJSONDataset: Any
+ParquetDataset: Any
 
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,

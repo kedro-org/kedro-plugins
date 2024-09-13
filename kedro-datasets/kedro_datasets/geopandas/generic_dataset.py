1d73dd4b0d74df6351136cd86e7d76f904647287
Harm Matthias Harms <matthias.harms@quis.de>
2024-09-13 10:56:58 +0200

    Use fiona for python < 3.11
    Signed-off-by: Harm Matthias Harms <matthias.harms@quis.de>

--- kedro-datasets/kedro_datasets/geopandas/generic_dataset.py
+++ kedro-datasets/kedro_datasets/geopandas/generic_dataset.py
@@ -6,6 +6,7 @@ allowed geopandas (pandas) options for loading and saving geosjon files.
 from __future__ import annotations
 
 import copy
+import sys
 from pathlib import PurePosixPath
 from typing import Any, Union
 
@@ -20,6 +21,9 @@ from kedro.io.core import (
 )
 
 NON_FILE_SYSTEM_TARGETS = ["postgis"]
+
+if sys.version_info < (3, 11):
+    gpd.options.io_engine = "fiona"
 
 
 class GenericDataset(

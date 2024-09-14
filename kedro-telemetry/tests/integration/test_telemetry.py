8aac41604658783962769fcae30c31786609cc38
Nok <nok.lam.chan@quantumblack.com>
2024-09-14 00:30:10 +0000

    add an obvious fail case, but still pass
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>

--- kedro-telemetry/tests/integration/test_telemetry.py
+++ kedro-telemetry/tests/integration/test_telemetry.py
@@ -5,6 +5,7 @@ from kedro.framework.cli.cli import KedroCLI
 from kedro.framework.session import KedroSession
 from kedro.framework.startup import bootstrap_project
 from pytest import fixture
+import pytest
 
 
 @fixture
@@ -37,7 +38,7 @@ class TestKedroTelemetryHookIntegration:
             "kedro_telemetry.plugin._check_for_telemetry_consent", return_value=True
         )
         kedro_cli = KedroCLI(dummy_project_path)
-        CliRunner().invoke(kedro_cli, ["info"])
+        CliRunner().invoke(kedro_cli, ["run"])
         mocked_heap_call.assert_called_once()
 
     def test_telemetry_sent_once_with_session_run(self, mocker, dummy_project_path):
@@ -57,4 +58,25 @@ class TestKedroTelemetryHookIntegration:
         bootstrap_project(dummy_project_path)
         with KedroSession.create(project_path=dummy_project_path) as session:
             session.run()
+        mocked_heap_call.assert_called_once()
+
+    @pytest.mark.xfail
+    def test_telemetry_sent_multi_with_other_kedro_command(
+        self, mocker, dummy_project_path
+    ):
+        from kedro_telemetry.plugin import telemetry_hook
+
+        telemetry_hook.consent = None
+        telemetry_hook._sent = False
+        telemetry_hook.event_properties = None
+        telemetry_hook.project_path = None
+
+        mocked_heap_call = mocker.patch("kedro_telemetry.plugin._send_heap_event")
+        mocker.patch(
+            "kedro_telemetry.plugin._check_for_telemetry_consent", return_value=True
+        )
+        kedro_cli = KedroCLI(dummy_project_path)
+        CliRunner().invoke(kedro_cli, ["run"])
+        CliRunner().invoke(kedro_cli, ["run"])
+        CliRunner().invoke(kedro_cli, ["run"])
         mocked_heap_call.assert_called_once()

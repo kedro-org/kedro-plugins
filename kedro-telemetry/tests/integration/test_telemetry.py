f4183e915f531e887464d56ea0cd23ba03885ff0
Dmitry Sorokin <129520297+DmitrySorokinQB@users.noreply.github.com>
2024-09-23 17:04:15 +0100

    Remove last test
    Signed-off-by: Dmitry Sorokin <129520297+DmitrySorokinQB@users.noreply.github.com>

--- kedro-telemetry/tests/integration/test_telemetry.py
+++ kedro-telemetry/tests/integration/test_telemetry.py
@@ -59,25 +59,3 @@ class TestKedroTelemetryHookIntegration:
         with KedroSession.create(project_path=dummy_project_path) as session:
             session.run()
         mocked_heap_call.assert_called_once()
-
-    def test_telemetry_sent_multi_with_other_kedro_command(
-        self, mocker, dummy_project_path
-    ):
-        from kedro_telemetry.plugin import telemetry_hook
-
-        telemetry_hook.consent = None
-        telemetry_hook._sent = False
-        telemetry_hook.event_properties = None
-        telemetry_hook.project_path = None
-
-        mocked_heap_call = mocker.patch("kedro_telemetry.plugin._send_heap_event")
-        mocker.patch(
-            "kedro_telemetry.plugin._check_for_telemetry_consent", return_value=True
-        )
-        kedro_cli = KedroCLI(dummy_project_path)
-        CliRunner().invoke(kedro_cli, ["run"])
-        telemetry_hook._sent = False
-        CliRunner().invoke(kedro_cli, ["run"])
-        telemetry_hook._sent = False
-        CliRunner().invoke(kedro_cli, ["run"])
-        mocked_heap_call.assert_called_once()

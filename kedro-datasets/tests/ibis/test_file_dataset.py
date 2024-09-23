0ce54f7f4a8d74351a98137c306fa921c7ff2f86
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-09-23 10:52:50 -0600

    feat(datasets): implement `FileDataset` versioning
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- kedro-datasets/tests/ibis/test_file_dataset.py
+++ kedro-datasets/tests/ibis/test_file_dataset.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+from time import sleep
+
 import ibis
 import pytest
+from kedro.io import DatasetError, Version
+from kedro.io.core import generate_timestamp
 from pandas.testing import assert_frame_equal
 
 from kedro_datasets.ibis import FileDataset
@@ -28,6 +33,16 @@ def file_dataset(filepath_csv, connection_config, load_args, save_args):
         connection=connection_config,
         load_args=load_args,
         save_args=save_args,
+    )
+
+
+@pytest.fixture
+def versioned_file_dataset(filepath_csv, connection_config, load_version, save_version):
+    return FileDataset(
+        filepath=filepath_csv,
+        file_format="csv",
+        connection=connection_config,
+        version=Version(load_version, save_version),
     )
 
 
@@ -95,3 +110,163 @@ class TestFileDataset:
         mocker.patch(f"ibis.{connection_config['backend']}")
         file_dataset.load()
         assert key in file_dataset._connections
+
+
+class TestFileDatasetVersioned:
+    def test_version_str_repr(self, connection_config, load_version, save_version):
+        """Test that version is in string representation of the class instance
+        when applicable."""
+        filepath = "test.csv"
+        ds = FileDataset(
+            filepath=filepath, file_format="csv", connection=connection_config
+        )
+        ds_versioned = FileDataset(
+            filepath=filepath,
+            file_format="csv",
+            connection=connection_config,
+            version=Version(load_version, save_version),
+        )
+        assert filepath in str(ds)
+        assert "version" not in str(ds)
+
+        assert filepath in str(ds_versioned)
+        ver_str = f"version=Version(load={load_version}, save='{save_version}')"
+        assert ver_str in str(ds_versioned)
+        assert "FileDataset" in str(ds_versioned)
+        assert "FileDataset" in str(ds)
+        # Default save_args
+        assert "save_args={}" in str(ds)
+        assert "save_args={}" in str(ds_versioned)
+
+    def test_save_and_load(self, versioned_file_dataset, dummy_table):
+        """Test that saved and reloaded data matches the original one for
+        the versioned data set."""
+        versioned_file_dataset.save(dummy_table)
+        reloaded = versioned_file_dataset.load()
+        assert_frame_equal(dummy_table.execute(), reloaded.execute())
+
+    def test_multiple_loads(
+        self, versioned_file_dataset, dummy_table, filepath_csv, connection_config
+    ):
+        """Test that if a new version is created mid-run, by an
+        external system, it won't be loaded in the current run."""
+        versioned_file_dataset.save(dummy_table)
+        versioned_file_dataset.load()
+        v1 = versioned_file_dataset.resolve_load_version()
+
+        sleep(0.5)
+        # force-drop a newer version into the same location
+        v_new = generate_timestamp()
+        FileDataset(
+            filepath=filepath_csv,
+            file_format="csv",
+            connection=connection_config,
+            version=Version(v_new, v_new),
+        ).save(dummy_table)
+
+        versioned_file_dataset.load()
+        v2 = versioned_file_dataset.resolve_load_version()
+
+        assert v2 == v1  # v2 should not be v_new!
+        ds_new = FileDataset(
+            filepath=filepath_csv,
+            file_format="csv",
+            connection=connection_config,
+            version=Version(None, None),
+        )
+        assert (
+            ds_new.resolve_load_version() == v_new
+        )  # new version is discoverable by a new instance
+
+    def test_multiple_saves(self, dummy_table, filepath_csv, connection_config):
+        """Test multiple cycles of save followed by load for the same dataset"""
+        ds_versioned = FileDataset(
+            filepath=filepath_csv,
+            file_format="csv",
+            connection=connection_config,
+            version=Version(None, None),
+        )
+
+        # first save
+        ds_versioned.save(dummy_table)
+        first_save_version = ds_versioned.resolve_save_version()
+        first_load_version = ds_versioned.resolve_load_version()
+        assert first_load_version == first_save_version
+
+        # second save
+        sleep(0.5)
+        ds_versioned.save(dummy_table)
+        second_save_version = ds_versioned.resolve_save_version()
+        second_load_version = ds_versioned.resolve_load_version()
+        assert second_load_version == second_save_version
+        assert second_load_version > first_load_version
+
+        # another dataset
+        ds_new = FileDataset(
+            filepath=filepath_csv,
+            file_format="csv",
+            connection=connection_config,
+            version=Version(None, None),
+        )
+        assert ds_new.resolve_load_version() == second_load_version
+
+    def test_no_versions(self, versioned_file_dataset):
+        """Check the error if no versions are available for load."""
+        pattern = r"Did not find any versions for FileDataset\(.+\)"
+        with pytest.raises(DatasetError, match=pattern):
+            versioned_file_dataset.load()
+
+    def test_exists(self, versioned_file_dataset, dummy_table):
+        """Test `exists` method invocation for versioned data set."""
+        assert not versioned_file_dataset.exists()
+        versioned_file_dataset.save(dummy_table)
+        assert versioned_file_dataset.exists()
+
+    def test_prevent_overwrite(self, versioned_file_dataset, dummy_table):
+        """Check the error when attempting to override the data set if the
+        corresponding CSV file for a given save version already exists."""
+        versioned_file_dataset.save(dummy_table)
+        pattern = (
+            r"Save path \'.+\' for FileDataset\(.+\) must "
+            r"not exist if versioning is enabled\."
+        )
+        with pytest.raises(DatasetError, match=pattern):
+            versioned_file_dataset.save(dummy_table)
+
+    @pytest.mark.parametrize(
+        "load_version", ["2019-01-01T23.59.59.999Z"], indirect=True
+    )
+    @pytest.mark.parametrize(
+        "save_version", ["2019-01-02T00.00.00.000Z"], indirect=True
+    )
+    def test_save_version_warning(
+        self, versioned_file_dataset, load_version, save_version, dummy_table
+    ):
+        """Check the warning when saving to the path that differs from
+        the subsequent load path."""
+        pattern = (
+            rf"Save version '{save_version}' did not match load version "
+            rf"'{load_version}' for FileDataset\(.+\)"
+        )
+        with pytest.warns(UserWarning, match=pattern):
+            versioned_file_dataset.save(dummy_table)
+
+    def test_versioning_existing_dataset(
+        self, file_dataset, versioned_file_dataset, dummy_table
+    ):
+        """Check the error when attempting to save a versioned dataset on top of an
+        already existing (non-versioned) dataset."""
+        file_dataset.save(dummy_table)
+        assert file_dataset.exists()
+        assert file_dataset._filepath == versioned_file_dataset._filepath
+        pattern = (
+            f"(?=.*file with the same name already exists in the directory)"
+            f"(?=.*{versioned_file_dataset._filepath.parent.as_posix()})"
+        )
+        with pytest.raises(DatasetError, match=pattern):
+            versioned_file_dataset.save(dummy_table)
+
+        # Remove non-versioned dataset and try again
+        Path(file_dataset._filepath.as_posix()).unlink()
+        versioned_file_dataset.save(dummy_table)
+        assert versioned_file_dataset.exists()

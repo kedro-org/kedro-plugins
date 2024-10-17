c2a980e69943ea0f1896179acfff1d28a694ea5d
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-10-18 01:10:58 +0530

    implemented the save() and load() funcs
    Signed-off-by: Minura Punchihewa <minurapunchihewa17@gmail.com>

--- kedro-datasets/kedro_datasets_experimental/safetensors/safetensors_dataset.py
+++ kedro-datasets/kedro_datasets_experimental/safetensors/safetensors_dataset.py
@@ -17,19 +17,85 @@ from kedro.io.core import (
 
 class SafetensorsDataset(AbstractVersionedDataset[Any, Any]):
 
+    DEFAULT_LOAD_ARGS: dict[str, Any] = {}
+    DEFAULT_SAVE_ARGS: dict[str, Any] = {}
+    DEFAULT_FS_ARGS: dict[str, Any] = {"open_args_save": {"mode": "wb"}}
+
     def __init__(  # noqa: PLR0913
         self,
         *,
         filepath: str,
-        backend: str = "torch",
+        backend: str = "pickle",
+        version: Version | None = None,
+        credentials: dict[str, Any] | None = None,
+        fs_args: dict[str, Any] | None = None,
+        metadata: dict[str, Any] | None = None,
     ) -> None:
-        pass
+        try:
+            importlib.import_module(f"safetensors.{backend}")
+        except ImportError as exc:
+            raise ImportError(
+                f"Selected backend '{backend}' could not be imported. "
+                "Make sure it is installed and importable."
+            ) from exc
+        
+        _fs_args = deepcopy(fs_args) or {}
+        _fs_open_args_load = _fs_args.pop("open_args_load", {})
+        _fs_open_args_save = _fs_args.pop("open_args_save", {})
+        _credentials = deepcopy(credentials) or {}
+
+        protocol, path = get_protocol_and_path(filepath, version)
+        if protocol == "file":
+            _fs_args.setdefault("auto_mkdir", True)
+
+        self._protocol = protocol
+        self._fs = fsspec.filesystem(self._protocol, **_credentials, **_fs_args)
+
+        self.metadata = metadata
+
+        super().__init__(
+            filepath=PurePosixPath(path),
+            version=version,
+            exists_function=self._fs.exists,
+            glob_function=self._fs.glob,
+        )
+
+        self._backend = backend
+
+        self._fs_open_args_load = {
+            **self.DEFAULT_FS_ARGS.get("open_args_load", {}),
+            **(_fs_open_args_load or {}),
+        }
+        self._fs_open_args_save = {
+            **self.DEFAULT_FS_ARGS.get("open_args_save", {}),
+            **(_fs_open_args_save or {}),
+        }
 
     def load(self) -> Any:
-        pass
+        load_path = get_filepath_str(self._get_load_path(), self._protocol)
+
+        with self._fs.open(load_path, **self._fs_open_args_load) as fs_file:
+            imported_backend = importlib.import_module(f"safetensors.{self._backend}")
+            return imported_backend.load(fs_file)
 
     def save(self, data: Any) -> None:
-        pass
+        save_path = get_filepath_str(self._get_save_path(), self._protocol)
+
+        with self._fs.open(save_path, **self._fs_open_args_save) as fs_file:
+            try:
+                imported_backend = importlib.import_module(f"safetensors.{self._backend}")
+                imported_backend.save_file(data, fs_file)
+            except Exception as exc:
+                raise DatasetError(
+                    f"{data.__class__} was not serialised due to: {exc}"
+                ) from exc
+            
+        self._invalidate_cache()
 
     def _exists(self) -> bool:
         pass
+
+    def _invalidate_cache(self) -> None:
+        """Invalidate underlying filesystem caches."""
+        filepath = get_filepath_str(self._filepath, self._protocol)
+        self._fs.invalidate_cache(filepath)

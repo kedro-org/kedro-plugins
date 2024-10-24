f4383c7368059aea61a3972d553d46e016354478
Merel Theisen <merel.theisen@quantumblack.com>
2024-10-24 10:58:34 +0100

    Skip instead of xfail
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- kedro-datasets/tests/snowflake/test_snowpark_dataset.py
+++ kedro-datasets/tests/snowflake/test_snowpark_dataset.py
@@ -5,7 +5,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 if sys.version_info >= (3, 12):
-    pytest.mark.xfail(
+    pytest.mark.skip(
         "Snowpark is not supported in Python versions higher than 3.11",
         allow_module_level=True,
     )

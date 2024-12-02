2e36bcdf291ccef7f1c5accde45f7bceb525471a
Huong Nguyen <huong.nguyen@mckinsey.com>
2024-12-02 13:59:38 +0000

    add deprecation warning for tracking datatsets
    Signed-off-by: Huong Nguyen <huong.nguyen@mckinsey.com>

--- kedro-datasets/kedro_datasets/tracking/__init__.py
+++ kedro-datasets/kedro_datasets/tracking/__init__.py
@@ -4,6 +4,9 @@ from typing import Any
 
 import lazy_loader as lazy
 
+import warnings
+from kedro_datasets import KedroDeprecationWarning
+
 # https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
 JSONDataset: Any
 MetricsDataset: Any
@@ -14,4 +17,10 @@ __getattr__, __dir__, __all__ = lazy.attach(
         "json_dataset": ["JSONDataset"],
         "metrics_dataset": ["MetricsDataset"],
     },
+)
+
+warnings.warn(
+    "`tracking.JSONDataset` and `tracking.MetricsDataset` are deprecated. These datasets will be removed in kedro-datasets 7.0.0",
+    KedroDeprecationWarning,
+    stacklevel=2,
 )

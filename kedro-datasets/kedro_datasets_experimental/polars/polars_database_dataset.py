6fc01a89c44f736ffaa55258719600ec9c5fafb0
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-01-14 23:29:30 +0530

    updated the implementation by extending SQLQueryDataset
    Signed-off-by: Minura Punchihewa <minurapunchihewa17@gmail.com>

--- kedro-datasets/kedro_datasets_experimental/polars/polars_database_dataset.py
+++ kedro-datasets/kedro_datasets_experimental/polars/polars_database_dataset.py
@@ -1,16 +1,14 @@
 import copy
+from pathlib import PurePosixPath
 from typing import Any, NoReturn
 
-import fsspec
 import polars as pl
-from kedro.io.core import (
-    AbstractDataset,
-    DatasetError,
-    get_protocol_and_path,
-)
+
+from kedro_datasets.pandas.sql_dataset import SQLQueryDataset, get_filepath_str
 
 
-class PolarsDatabaseDataset(AbstractDataset[None, pl.DataFrame]):
+class PolarsDatabaseDataset(SQLQueryDataset):
+
     def __init__(  # noqa: PLR0913
         self,
         sql: str | None = None,
@@ -18,60 +16,33 @@ class PolarsDatabaseDataset(AbstractDataset[None, pl.DataFrame]):
         load_args: dict[str, Any] | None = None,
         fs_args: dict[str, Any] | None = None,
         filepath: str | None = None,
-        execution_options: dict[str, Any] | None = None,
         metadata: dict[str, Any] | None = None,
     ) -> None:
-        if sql and filepath:
-            raise DatasetError(
-                "'sql' and 'filepath' arguments cannot both be provided."
-                "Please only provide one."
-            )
-
-        if not (sql or filepath):
-            raise DatasetError(
-                "'sql' and 'filepath' arguments cannot both be empty."
-                "Please provide a sql query or path to a sql query file."
-            )
-
-        if not (credentials and "con" in credentials and credentials["con"]):
-            raise DatasetError(
-                "'con' argument cannot be empty. Please "
-                "provide a SQLAlchemy connection string."
-            )
-
-        default_load_args: dict[str, Any] = {}
-
-        self._load_args = (
-            {**default_load_args, **load_args}
-            if load_args is not None
-            else default_load_args
+        """Creates a new ``PolarsDatabaseDataset``."""
+        super().__init__(
+            sql=sql,
+            credentials=credentials,
+            load_args=load_args,
+            fs_args=fs_args,
+            filepath=filepath,
+            metadata=metadata,
         )
 
-        self.metadata = metadata
-
-        # load sql query from file
-        if sql:
-            self._load_args["sql"] = sql
-            self._filepath = None
-        else:
-            # filesystem for loading sql file
-            _fs_args = copy.deepcopy(fs_args) or {}
-            _fs_credentials = _fs_args.pop("credentials", {})
-            protocol, path = get_protocol_and_path(str(filepath))
-
-            self._protocol = protocol
-            self._fs = fsspec.filesystem(self._protocol, **_fs_credentials, **_fs_args)
-            self._filepath = path
-        self._connection_str = credentials["con"]
-        self._connection_args = {
-            k: credentials[k] for k in credentials.keys() if k != "con"
-        }
-        self._execution_options = execution_options or {}
-        if "mssql" in self._connection_str:
-            self.adapt_mssql_date_params()
-
     def load(self) -> pl.DataFrame:
-        pass
+        load_args = copy.deepcopy(self._load_args)
+
+        if self._filepath:
+            load_path = get_filepath_str(PurePosixPath(self._filepath), self._protocol)
+            with self._fs.open(load_path, mode="r") as fs_file:
+                query = fs_file.read()
+        else:
+            query = load_args.pop("sql")
+
+        return pl.read_database(
+            query=query,
+            connection=self._connection_str,
+            **load_args
+        )
 
     def save(self, data: None) -> NoReturn:
         pass

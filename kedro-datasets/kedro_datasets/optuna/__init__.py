bb7d6b99799ee8d05c8d5c9725a7783fac996c5e
Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>
2025-02-25 17:52:46 +0100

    Add StudyDataset
    Signed-off-by: Guillaume Tauzin <4648633+gtauzin@users.noreply.github.com>

---
+++ kedro-datasets/kedro_datasets/optuna/__init__.py
@@ -0,0 +1,11 @@
+"""Provide data loading and saving functionality for Optuna's study."""
+
+from typing import Any
+
+import lazy_loader as lazy
+
+StudyDataset: Any
+
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__, submod_attrs={"study": ["StudyDataset"],}
+)

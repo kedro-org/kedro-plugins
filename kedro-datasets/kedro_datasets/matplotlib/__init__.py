aa62a6422455d2959cf6b21b2f209df1ff09127d
Sajid Alam <sajid_alam@mckinsey.com>
2025-03-04 15:40:45 +0000

    rename and deprecate old matplotlib_writer
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- kedro-datasets/kedro_datasets/matplotlib/__init__.py
+++ kedro-datasets/kedro_datasets/matplotlib/__init__.py
@@ -5,7 +5,12 @@ from typing import Any
 import lazy_loader as lazy
 
 MatplotlibWriter: Any
+MatplotlibDataset: Any
 
 __getattr__, __dir__, __all__ = lazy.attach(
-    __name__, submod_attrs={"matplotlib_writer": ["MatplotlibWriter"]}
+    __name__,
+    submod_attrs={
+        "matplotlib_writer": ["MatplotlibWriter"],
+        "matplotlib_dataset": ["MatplotlibDataset"]
+    }
 )

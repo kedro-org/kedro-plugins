7acc739e6406bb8d86ef2c43ede6408795259ceb
Merel Theisen <merel.theisen@quantumblack.com>
2025-03-10 17:47:24 +0100

    Clean up & verify
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- kedro-datasets/tests/databricks/test_managed_table_dataset.py
+++ kedro-datasets/tests/databricks/test_managed_table_dataset.py
@@ -1,12 +1,4 @@
-import pytest
 from kedro_datasets.databricks import ManagedTableDataset
-
-# importlib_metadata needs backport for python 3.8 and older
-import importlib_metadata
-from pyspark.sql import SparkSession
-
-DELTA_VERSION = importlib_metadata.version("delta-spark")
-
 
 class TestManagedTableDataset:
     def test_describe(self):

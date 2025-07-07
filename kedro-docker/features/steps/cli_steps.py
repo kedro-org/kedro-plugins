fbcba22229c51ae842f102ed3a810d3792fe8bdb
Elena Khaustova <ymax70rus@gmail.com>
2025-07-07 16:51:49 +0100

    Removed logging.yml overwriting
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro-docker/features/steps/cli_steps.py
+++ kedro-docker/features/steps/cli_steps.py
@@ -2,7 +2,6 @@
 
 import re
 import sys
-import textwrap
 from pathlib import Path
 from time import sleep
 
@@ -145,36 +144,6 @@ def create_project_from_config_file(context, starter_name):
         env=context.env,
         cwd=str(context.temp_dir),
     )
-
-    # # override base logging configuration to simplify assertions
-    # logging_conf = context.root_project_dir / "conf" / "logging.yml"
-    # logging_conf.write_text(
-    #     textwrap.dedent(
-    #         """
-    #     version: 1
-    #
-    #     disable_existing_loggers: False
-    #
-    #     formatters:
-    #       simple:
-    #         format: "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    #
-    #     handlers:
-    #       console:
-    #         class: logging.StreamHandler
-    #         level: INFO
-    #         formatter: simple
-    #         stream: ext://sys.stdout
-    #
-    #     loggers:
-    #       kedro:
-    #         level: INFO
-    #
-    #     root:
-    #       handlers: [console]
-    #     """
-    #     )
-    # )
 
     assert res.returncode == 0
 

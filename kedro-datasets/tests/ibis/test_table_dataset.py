0af5b821c38d8a2fb4a93383f35ac0a9e1ac7d16
gitgud5000 <17186026+gitgud5000@users.noreply.github.com>
2025-09-10 03:18:53 -0400

    fix(tests): replace realistic DB credentials in ibis table tests to avoid CI secret detection
    Signed-off-by: gitgud5000 <17186026+gitgud5000@users.noreply.github.com>

--- kedro-datasets/tests/ibis/test_table_dataset.py
+++ kedro-datasets/tests/ibis/test_table_dataset.py
@@ -382,24 +382,24 @@ class TestTableDataset:
         ("credentials_config", "key"),
         [
             (
-                "postgres://user:pwd@xxxx.postgres.database.azure.com:5432/postgres",
+                "postgres://xxxxx:yyyyyy@xxxx.postgres.database.azure.com:5432/postgres",
                 (
                     ("backend", "postgres"),
                     (
                         "con",
-                        "postgres://user:pwd@xxxx.postgres.database.azure.com:5432/postgres",
+                        "postgres://xxxxx:yyyyyy@xxxx.postgres.database.azure.com:5432/postgres",
                     ),
                 ),
             ),
             (
                 {
-                    "con": "postgres://user:pwd@xxxx.postgres.database.azure.com:5432/postgres"
+                    "con": "postgres://xxxxx:yyyyyy@xxxx.postgres.database.azure.com:5432/postgres"
                 },
                 (
                     ("backend", "postgres"),
                     (
                         "con",
-                        "postgres://user:pwd@xxxx.postgres.database.azure.com:5432/postgres",
+                        "postgres://xxxxx:yyyyyy@xxxx.postgres.database.azure.com:5432/postgres",
                     ),
                 ),
             ),
@@ -455,7 +455,7 @@ class TestTableDataset:
 
         def test_get_backend_name_string_with_scheme(self):
             ds = TableDataset(table_name="t")
-            ds._credentials = "mysql://user:pass@host:3306/dbname"
+            ds._credentials = "mysql://uxxxxx:yyyyyy@host:3306/dbname"
             assert ds._get_backend_name() == "mysql"
 
         def test_get_backend_name_string_without_scheme(self):

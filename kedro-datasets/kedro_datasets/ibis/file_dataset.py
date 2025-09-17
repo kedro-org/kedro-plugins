25ad453f98c1c52b7312735e00c6e3cbf244a131
Elena Khaustova <ymax70rus@gmail.com>
2025-09-17 17:53:29 +0100

    Restored tests
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro-datasets/kedro_datasets/ibis/file_dataset.py
+++ kedro-datasets/kedro_datasets/ibis/file_dataset.py
@@ -48,8 +48,6 @@ class FileDataset(ConnectionMixin, AbstractVersionedDataset[ir.Table, ir.Table])
         Using the [Python API](https://docs.kedro.org/en/stable/catalog-data/advanced_data_catalog_usage/):
 
         >>> import ibis
-        >>> import pyarrow as pa
-        >>>
         >>> from kedro_datasets.ibis import FileDataset
         >>>
         >>> data = ibis.memtable({"col1": [1, 2], "col2": [4, 5], "col3": [5, 6]})
@@ -62,16 +60,8 @@ class FileDataset(ConnectionMixin, AbstractVersionedDataset[ir.Table, ir.Table])
         ... )
         >>> dataset.save(data)
         >>> reloaded = dataset.load()
-        >>>
-        >>> def _normalize_arrow_result(x):
-        ...     # If backend returns a RecordBatchReader, materialize it
-        ...     if hasattr(x, "read_all") and callable(x.read_all):
-        ...         return x.read_all()
-        ...     return x
-        >>>
-        >>> left = _normalize_arrow_result(data.execute())
-        >>> right = _normalize_arrow_result(reloaded.execute())
-        >>> assert left.to_pandas().equals(right.to_pandas())
+        >>> assert data.execute().equals(reloaded.execute())
+
     """
 
     DEFAULT_CONNECTION_CONFIG: ClassVar[dict[str, Any]] = {

753f68259f5ea18016c197d5fcf5f22c8a9da3b5
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2025-11-13 14:17:00 +0000

    Update init
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- kedro-datasets/kedro_datasets/langchain/__init__.py
+++ kedro-datasets/kedro_datasets/langchain/__init__.py
@@ -5,9 +5,27 @@ import lazy_loader as lazy
 
 try:
     from .chat_anthropic_dataset import ChatAnthropicDataset
+except (ImportError, RuntimeError):
+    # For documentation builds that might fail due to dependency issues
+    ChatAnthropicDataset: Any
+
+try:
     from .chat_cohere_dataset import ChatCohereDataset
+except (ImportError, RuntimeError):
+    # For documentation builds that might fail due to dependency issues
+    ChatCohereDataset: Any
+
+try:
     from .chat_openai_dataset import ChatOpenAIDataset
-    from .openai_embeddings_dataset OpenAIEmbeddingsDataset
+except (ImportError, RuntimeError):
+    # For documentation builds that might fail due to dependency issues
+    ChatOpenAIDataset: Any
+
+try:  
+    from .openai_embeddings_dataset import OpenAIEmbeddingsDataset
+except (ImportError, RuntimeError):
+    # For documentation builds that might fail due to dependency issues
+    OpenAIEmbeddingsDataset: Any
 
 except (ImportError, RuntimeError):
     # For documentation builds that might fail due to dependency issues

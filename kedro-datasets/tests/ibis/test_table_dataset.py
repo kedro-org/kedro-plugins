b0b7f59a808a8d8053ddaf2357e9a251331c87c4
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-12-21 22:07:20 -0700

    test(datasets): clean up the Claude-generated test
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- kedro-datasets/tests/ibis/test_table_dataset.py
+++ kedro-datasets/tests/ibis/test_table_dataset.py
@@ -140,36 +140,30 @@ class TestTableDataset:
         Version(ibis.__version__) < Version("9.0.0"),
         reason='Ibis 9.0 standardised use of "database" to mean a collection of tables',
     )
-    @pytest.mark.parametrize("database_name", ["my_schema"], indirect=True)
     @pytest.mark.parametrize(
-        "save_args", [{"materialized": "table", "mode": "append"}], indirect=True
+        ("database_name", "save_args"),
+        [("test", {"materialized": "table", "mode": "append"})],
+        indirect=True,
     )
     def test_save_mode_append_with_database(
         self, table_dataset, database_name, dummy_table
     ):
-        """Saving with mode=append should work with non-default database/schema.
+        """Saving with mode=append should work with non-default database.
 
-        This test verifies the fix for the issue where _exists() doesn't check
-        the correct schema when database parameter is specified.
+        This test verifies the fix for the issue where exists() doesn't
+        check the correct schema when database parameter is specified;
+        see https://github.com/kedro-org/kedro-plugins/issues/1265
+
         """
-        # Create a non-default schema/database
+        # Create a non-default database.
         table_dataset.connection.create_database(database_name)
 
-        # First save - creates the table
         df1 = dummy_table
-        table_dataset.save(df1)
-
-        # Verify table exists in the correct schema
-        assert table_dataset._exists()
-        assert table_dataset._table_name in table_dataset.connection.list_tables(
-            database=database_name
-        )
-
-        # Second save - should append, not fail
         df2 = dummy_table
+
+        table_dataset.save(df1)
         table_dataset.save(df2)
 
-        # Verify data was appended correctly
         df1 = df1.execute()
         df2 = df2.execute()
         reloaded = table_dataset.load().execute()
@@ -344,8 +338,6 @@ class TestTableDataset:
         table_dataset.connection.create_database(database_name)
 
         table_dataset.save(dummy_table)
-        reloaded = table_dataset.load()
-        assert_frame_equal(dummy_table.execute(), reloaded.execute())
 
         # Verify that the attached database file was the one written to.
         con = duckdb.connect(database)

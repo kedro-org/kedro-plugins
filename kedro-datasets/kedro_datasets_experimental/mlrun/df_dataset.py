9a95497b90558c30f2d11032e8916741a7be40e2
rokatyy <ekaterina_molchanova@mckinsey.com>
2026-02-05 11:29:24 +0000

    fix
    Signed-off-by: rokatyy <ekaterina_molchanova@mckinsey.com>

--- kedro-datasets/kedro_datasets_experimental/mlrun/df_dataset.py
+++ kedro-datasets/kedro_datasets_experimental/mlrun/df_dataset.py
@@ -42,10 +42,12 @@ class MLRunDataframeDataset(MLRunAbstractDataset):
         save_args: Passed to log_artifact; see MLRun docs for your version.
     """
 
-    def __init__(self,
-                 key,
-                 load_args: dict[str, Any] | None = None,
-                 save_args: dict[str, Any] | None = None, ) -> None:
+    def __init__(
+        self,
+        key: str | None = None,
+        load_args: dict[str, Any] | None = None,
+        save_args: dict[str, Any] | None = None,
+    ) -> None:
         super().__init__(key=key, save_args=save_args, load_args=load_args)
 
     def load(self):

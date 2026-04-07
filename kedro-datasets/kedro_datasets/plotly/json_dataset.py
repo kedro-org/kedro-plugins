0cae3a76409ca25c76e052977597fc8e4521e19b
datascienceio <prachee.choudhury.07@gmail.com>
2026-04-07 19:09:05 +1000

    Update filepath type to support os.PathLike
    Signed-off-by: datascienceio <prachee.choudhury.07@gmail.com>

--- kedro-datasets/kedro_datasets/plotly/json_dataset.py
+++ kedro-datasets/kedro_datasets/plotly/json_dataset.py
@@ -2,7 +2,7 @@
 filesystem (e.g.: local, S3, GCS).
 """
 from __future__ import annotations
-
+import os
 import json
 from copy import deepcopy
 from pathlib import PurePosixPath
@@ -59,7 +59,7 @@ class JSONDataset(AbstractVersionedDataset[go.Figure, go.Figure | go.FigureWidge
     def __init__(  # noqa: PLR0913
         self,
         *,
-        filepath: str,
+        filepath: str | os.PathLike,
         load_args: dict[str, Any] | None = None,
         save_args: dict[str, Any] | None = None,
         version: Version | None = None,

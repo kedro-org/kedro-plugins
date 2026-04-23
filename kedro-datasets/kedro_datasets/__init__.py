cbf9dee2796f57c5b5423c2bb94304eb4d8a437b
Laura Couto <laurarccouto@gmail.com>
2026-04-23 15:25:12 -0300

    Try to suppress warning on __init__
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

--- kedro-datasets/kedro_datasets/__init__.py
+++ kedro-datasets/kedro_datasets/__init__.py
@@ -6,13 +6,16 @@ __version__ = "9.3.0"
 import sys
 import warnings
 
-try:
-    # Custom `KedroDeprecationWarning` class was added in Kedro 0.18.14.
-    from kedro import KedroDeprecationWarning
-except ImportError:  # pragma: no cover
+with warnings.catch_warnings():
+    # TODO: remove once Kedro releases Python 3.14 support
+    warnings.filterwarnings("ignore", "Kedro is not yet fully compatible")
+    try:
+        # Custom `KedroDeprecationWarning` class was added in Kedro 0.18.14.
+        from kedro import KedroDeprecationWarning
+    except ImportError:  # pragma: no cover
 
-    class KedroDeprecationWarning(DeprecationWarning):  # type: ignore[no-redef]
-        """Custom class for warnings about deprecated Kedro features."""
+        class KedroDeprecationWarning(DeprecationWarning):  # type: ignore[no-redef]
+            """Custom class for warnings about deprecated Kedro features."""
 
 
 if not sys.warnoptions:

7c217d37ed7601c059246a077f11bb4146a6d2fb
Laura Couto <laurarccouto@gmail.com>
2026-04-23 22:06:48 -0300

    Skip tensorflow tests on class and not module level
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

--- kedro-datasets/tests/tensorflow/test_tensorflow_model_dataset.py
+++ kedro-datasets/tests/tensorflow/test_tensorflow_model_dataset.py
@@ -15,11 +15,10 @@ if sys.platform == "win32":
         allow_module_level=True,
     )
 
-if sys.version_info >= (3, 14):
-    pytest.skip(
-        "TensorFlow does not support Python 3.14",
-        allow_module_level=True,
-    )
+_skip_on_314 = pytest.mark.skipif(
+    sys.version_info >= (3, 14),
+    reason="TensorFlow does not support Python 3.14",
+)
 
 
 # In this test module, we wrap tensorflow and TensorFlowModelDataset imports into a module-scoped
@@ -144,6 +143,7 @@ def dummy_tf_subclassed_model(dummy_x_train, dummy_y_train, tf):
     return model
 
 
+@_skip_on_314
 class TestTensorFlowModelDataset:
     """No versioning passed to creator"""
 
@@ -281,6 +281,7 @@ class TestTensorFlowModelDataset:
         assert len(dummy_tf_base_model_new.layers) == len(reloaded.layers)
 
 
+@_skip_on_314
 class TestTensorFlowModelDatasetVersioned:
     """Test suite with versioning argument passed into TensorFlowModelDataset creator"""
 
